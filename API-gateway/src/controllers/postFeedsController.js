0f08e029fa67db95f87cd822e16491cacf4f0328
humbejeff2116 <humbejeff2116@gmail.com>
2021-05-29 13:34:33 +0100

    started writing JSDocs on gateway postFeedsController

--- API-gateway/src/controllers/postFeedsController.js
+++ API-gateway/src/controllers/postFeedsController.js
@@ -17,30 +17,56 @@
 
 
 
-
+/**
+ * @class 
+ *  products and service controller class 
+ * @module PostFeedController
+ */
 function PostFeedController() {
     this.postFeedClient;
     this.gatewayServerSocket;
 }
 
+/**
+ * @method mountSocket 
+ * 
+ ** Used to initialize the class instance variables
+ * @param {object} postFeedClient - the socket.IO client of the postFeed controller class
+ * @param {object} gatewayServerSocket - the socket.IO server socket of the gateway server
+ * 
+ */
 PostFeedController.prototype.mountSocket = function({ postFeedClient, gatewayServerSocket }) {
     this.postFeedClient = postFeedClient;
     this.gatewayServerSocket = gatewayServerSocket
     return this;
 }
 
+/**
+ * @method postFeed 
+ ** used to create a post feed
+ ** initiates a client server connection with post feed service node
+ ** collects post feed data from frontend/client and sends to post feed service node
+ ** sends back created post feed recieved from post feed service to frontend/cient
+ * @param {object} data - the postFeed data collected from the front end 
+ */
 PostFeedController.prototype.postFeed = function(data = {}) {
     const self = this;
-    // emit postfeed data from gateway to postfeed service
     this.postFeedClient.emit('postFeed', data);
-    // receieve posted feed from postfeed server node
+
     this.postFeedClient.on('feedPosted', function(response) {
-        // send postfeed to frontend
         self.gatewayServerSocket.emit('feedPosted', response);
         console.log(response);
     });   
 }
 
+/**
+ * @method getUserPostedFeeds
+ ** used to get a user post feed
+ ** initiates a client server connection with post feed service node
+ ** collects user data from frontend/client and sends to post feed service node
+ ** sends back user post feed recieved from post feed service to frontend/cient
+ * @param {object} data - the user data collected from the front end
+ */
 PostFeedController.prototype.getUserPostedFeeds = function(data = {}) {
     const self = this;
     this.postFeedClient.emit('getUserPostedFeeds', data);
@@ -51,22 +77,38 @@ PostFeedController.prototype.getUserPostedFeeds = function(data = {}) {
     });   
 }
 
+/**
+ * @method starPostFeed 
+ ** used to give a star on a user post feed
+ ** initiates a client server connection with post feed service node
+ ** collects  data from frontend/client and sends to post feed service node
+ ** sends back postfeed not found error if ecountered from post feed service to frontend/cient
+ ** sends back response after successfully placing a star to frontend/cient
+ * @param {object} data - the user data collected from the front end which includes the user and post feed 
+ */
 PostFeedController.prototype.starPostFeed = function(data = {}) {
     const self = this;
-    // emit postfeed data from gateway to postfeed service
     this.postFeedClient.emit('starPostFeed', data);
-    // send postfeed not found error to frontend
+
     this.postFeedClient.on('starFeedError', function(response) {
         self.gatewayServerSocket.emit('starFeedError', response)
     })
-    // receieve posted feed after successfully placing a star from postfeed server node
+   
     this.postFeedClient.on('starPostFeedSuccess', function(response) {
-        // send postfeed after placing a star to frontend
         self.gatewayServerSocket.emit('starPostFeedSuccess', response);
         console.log(response);
     });   
 }
 
+/**
+ * @method unStarPostFeed
+ ** used to remove a star on a user post feed
+ ** initiates a client server connection with post feed service node
+ ** collects  data from frontend/client and sends to post feed service node
+ ** sends back postfeed not found error if ecountered from post feed service to frontend/cient
+ ** sends back response after successfully removing a star from postfeed server node to frontend/cient
+ * @param {object} data - the user data collected from the front end which includes the user and post feed  
+ */
 PostFeedController.prototype.unStarPostFeed = function(data = {}) {
     const self = this;
     this.postFeedClient.emit('unStarPostFeed', data);
@@ -81,6 +123,15 @@ PostFeedController.prototype.unStarPostFeed = function(data = {}) {
     });   
 }
 
+/**
+ * @method commentOnPostFeed
+ ** used to comment on a user post feed
+ ** initiates a client server connection with post feed service node
+ ** collects  data from frontend/client and sends to post feed service node
+ ** sends back postfeed not found error if ecountered from post feed service to frontend/cient
+ ** sends back response after successfully commenting on a user postfeed to frontend/cient
+ * @param {object} data - the user data collected from the front end which includes the user and comment  
+ */
 PostFeedController.prototype.commentOnPostFeed = function(data ={}) {
     let d = {
         comment:'ddjfdjfhdj',
@@ -89,7 +140,6 @@ PostFeedController.prototype.commentOnPostFeed = function(data ={}) {
         }
     }
     const self = this;
-
     this.postFeedClient.emit('commentOnPostFeed', data);
 
     this.postFeedClient.on('commentOnPostFeedError', function(response) {
