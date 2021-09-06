c4f0ae42be901b8f5bca882e8001755a057cf9ac
humbejeff2116 <humbejeff2116@gmail.com>
2021-09-06 10:01:36 +0100

    modified socket io connections file

--- API-gateway/src/utils/socketConnections.js
+++ API-gateway/src/utils/socketConnections.js
@@ -2,18 +2,18 @@
 
 
 
+const socketio_client = require('socket.io-client')
 
-
-const userClient = require('socket.io-client')('http://localhost:4001');
+const userClient = socketio_client('http://localhost:4001',{reconnection: true, reconnectionDelay: 10000});
 userClient.on('connect', function() {
    userClient.sendBuffer = [];
    console.log("user client has connected")
 });
-const dataMergerClient = require('socket.io-client')('http://localhost:4002');
-const postFeedClient = require('socket.io-client')('http://localhost:4006');
-const orderClient = require('socket.io-client')('http://localhost:4004');
-const accountActivityClient = require('socket.io-client')('http://localhost:4005');
-const productClient =  require('socket.io-client')('http://localhost:4003');
+const dataMergerClient = socketio_client('http://localhost:4002',{reconnection: true, reconnectionDelay: 10000});
+const postFeedClient = socketio_client('http://localhost:4006',{reconnection: true, reconnectionDelay: 10000});
+const orderClient = socketio_client('http://localhost:4004',{reconnection: true, reconnectionDelay: 10000});
+const accountActivityClient = socketio_client('http://localhost:4005',{reconnection: true, reconnectionDelay: 10000});
+const productClient =  socketio_client('http://localhost:4003',{reconnection: true, reconnectionDelay: 10000});
 
 const socketOptions = {
     userClient: userClient,
