ba36077ea2b6d47957369f94dd2040431cc00982
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-08 17:07:14 +0000

    export UserDataChangeController and FeesController from index file

--- API-gateway/src/controllers/socketControllers/index.js
+++ API-gateway/src/controllers/socketControllers/index.js
@@ -10,11 +10,14 @@ const PostFeedsController = require('./postFeedsController');
 const ProductController = require('./productsController');
 const ProductOrderController = require('./orderController');
 const ProductCommentController = require('./productCommentController');
-
+const UserDataChangeController = require('./userDataChangeController');
+const FeesController = require('./feesController');
 module.exports = {
     UserController,
     PostFeedsController,
     ProductController,
     ProductOrderController,
     ProductCommentController,
+    UserDataChangeController,
+    FeesController
 }
