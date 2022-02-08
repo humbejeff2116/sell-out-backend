a31d5622f9debc3ccb96d7d06c825b4646a01b66
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-08 17:35:30 +0000

    export userDataChange socket events handler and order socket events handler from index file

--- API-gateway/src/libs/socketEventsHandlers/index.js
+++ API-gateway/src/libs/socketEventsHandlers/index.js
@@ -7,11 +7,15 @@ const {userSocketEventsHandler} = require('./userSocketEventsHandler');
 const {productSocketEventsHandler} = require('./productSocketEventHandler');
 const {productCommentSocketEventsHandler} = require('./productCommentSocketEventsHandler')
 const {orderSocketEventsHandler} = require('./orderSocketEventHandler');
+const { userDataChangeSocketEventsHandler } = require('./userDataChangeSocketEventsHandler');
+const { feesSocketEventsHandler } = require('./feesSocketEventsHandler');
 
 
 module.exports = {
     userSocketEventsHandler,
     productSocketEventsHandler,
     productCommentSocketEventsHandler,
-    orderSocketEventsHandler
+    orderSocketEventsHandler,
+    userDataChangeSocketEventsHandler,
+    feesSocketEventsHandler 
 }
