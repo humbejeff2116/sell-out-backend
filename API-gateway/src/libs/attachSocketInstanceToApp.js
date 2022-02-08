19acab7ad6325d9085ead909b92bdb2878715a96
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-08 17:38:05 +0000

    refactor function to attach socket class instance to express app

--- API-gateway/src/libs/attachSocketInstanceToApp.js
+++ API-gateway/src/libs/attachSocketInstanceToApp.js
@@ -1,15 +1,7 @@
 
 function attachSocketInstanceToApp(app, io, socket) {
-    function HTTPSocketManger() {
-        this.io;
-        this.socket;
-    }
-    HTTPSocketManger.prototype.setSocketDetails = function(io, socket) {
-        this.io = io;
-        this.socket = socket;
-    }
-    const HTTPSocketInstance = new HTTPSocketManger();
-    HTTPSocketInstance.setSocketDetails(io, socket);
-    app.set("socketInstance", HTTPSocketInstance)
+    
+    app.set("socketInstance", { io,socket })
+
 }
-exports.attachSocketInstanceToApp = attachSocketInstanceToApp;
+module.exports.attachSocketInstanceToApp = attachSocketInstanceToApp;
