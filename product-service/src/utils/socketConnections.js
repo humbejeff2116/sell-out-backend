0e8c8b9d2f1f4c90eec093af0446faeae5160901
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-11 21:54:13 +0000

    clean socketConnections code

--- product-service/src/utils/socketConnections.js
+++ product-service/src/utils/socketConnections.js
@@ -8,12 +8,11 @@
 const userClient = require('socket.io-client')('http://localhost:4001');
 userClient.on('connect', function() {
    userClient.sendBuffer = [];
-   console.log("user client has connected from product service")
+   console.log(`${"product service"} ---> ${"account service"} status: ${"connected"}`)
 });
  const socketOptions = {
-    serverSocket: null,
-    // gatewayClient: require('socket.io-client')('http://localhost:4003'),
-    userClient:userClient,
+   serverSocket: null,
+   userClient: userClient,
 }
 
  module.exports = socketOptions
