cb2897c3d8e7792cf86be929ea2637c668ba516d
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-11 21:49:01 +0000

    clean socketConnections code

--- fees-service/src/utils/socketConnections.js
+++ fees-service/src/utils/socketConnections.js
@@ -7,10 +7,15 @@
 
 const userClient = require('socket.io-client')('http://localhost:4001');
 
+userClient.on('connect', function() {
+  userClient.sendBuffer = [];
+  console.log(`${"fees service"} ---> ${"account service"} status: ${"connected"}`)
+});
 
- const socketOptions = {
-   serverSocket: null,
-   userClient: userClient,
+
+const socketOptions = {
+  serverSocket: null,
+  userClient: userClient,
 }
 
  module.exports = socketOptions
