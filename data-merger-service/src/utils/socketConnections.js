eedd9daeb8c201cae44ace20abcfe83f9bc63e00
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-13 20:31:47 +0000

    clean socket connection code

--- data-merger-service/src/utils/socketConnections.js
+++ data-merger-service/src/utils/socketConnections.js
@@ -6,12 +6,12 @@
 
 
 const userClient = require('socket.io-client')('http://localhost:4001');
+userClient.on('connect', function() {
+    userClient.sendBuffer = [];
+    console.log(`${"data merger service"} ---> ${"account service"} status: ${"connected"}`)
+});
+
 const productClient = require('socket.io-client')('http://localhost:4003');
-// const userClient = require('socket.io-client')('http://localhost:4001');
-// userClient.on('connect', function() {
-//    userClient.sendBuffer = [];
-//    console.log("user client has connected from product service")
-// });
  const socketOptions = {
    serverSocket: null,
    userClient: userClient,
