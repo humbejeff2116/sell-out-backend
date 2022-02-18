80f18f61d959012bed99eecfe9b63c9b16bb7b10
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-18 19:16:15 +0000

    implement getUserPayments socket controller function

--- API-gateway/src/controllers/socketControllers/feesController.js
+++ API-gateway/src/controllers/socketControllers/feesController.js
@@ -37,7 +37,23 @@
 
 
 
+FeesController.prototype.getUserPayments = function(data) {
+    this.feesClient.emit('getUserPayments', data); 
+}
+FeesController.prototype.getUserPaymentsResponse = function(io) {
 
+    this.feesClient.on('getUserPaymentsError', function (response) {
+        const { socketId, ...rest } = response;
+        io.to(socketId).emit('getUserProductOrdersError', response);
+        console.log(response);
+    });
+    this.feesClient.on('getUserPaymentsSuccess', function (response) {
+        const { socketId, ...rest } = response;
+        io.to(socketId).emit('getUserPaymentsSuccess', response);
+    }); 
+}
+
+// response from fees sevice after  creating user order payment
 FeesController.prototype.createProductOrderPaymentResponse = function(io) {
     
     this.feesClient.on('createPaymentSuccess', function(response) {
