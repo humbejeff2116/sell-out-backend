54569e88f9cb1f5c550f28305b60b0eb6c6f3682
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-18 20:14:44 +0000

    clean socket connection and disconnection message (gateway)

--- API-gateway/src/libs/socketMessage.js
+++ API-gateway/src/libs/socketMessage.js
@@ -5,33 +5,50 @@
 
 
 const message = (function() {
+
     let numberOfOnlineUsers = 0;
-    return({
-        socketConnectionMessage,
-        socketDisconnetionMessage
-    });
+    
+    return({ socketConnectionMessage, socketDisconnetionMessage })
+
     function socketConnectionMessage(socket) {
+
+        let message;
         ++numberOfOnlineUsers
-        console.log("New client connected --> " + socket.id);
-        let message;
+        console.log("New socket connection --> id:" + socket.id);
+        
         if (numberOfOnlineUsers < 2) {
+
             message = `${numberOfOnlineUsers} user online`
+
         } else {
-        message = `${numberOfOnlineUsers} users online`
+
+            message = `${numberOfOnlineUsers} users online`
+
         }
+
         console.log(message);
+
     }
+
     function socketDisconnetionMessage(socket) {
-        --numberOfOnlineUsers
-        console.log("client disconnected --> " + socket.id);
+
         let message;
+        --numberOfOnlineUsers
+        console.log("socket disconnected --> id:" + socket.id);
+        
         if (numberOfOnlineUsers < 2) {
+
             message = `${numberOfOnlineUsers} user online`
+
         } else {
-        message = `${numberOfOnlineUsers} users online`
+
+            message = `${numberOfOnlineUsers} users online`
+
         }
+
         console.log(message);
     }
+
 })()
 
 module.exports = message;
