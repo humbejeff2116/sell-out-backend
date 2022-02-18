8884446fe8627649996738241422569b3407da05
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-18 19:22:45 +0000

    add getUserPayment event to fees socket events handler

--- API-gateway/src/libs/socketEventsHandlers/feesSocketEventsHandler.js
+++ API-gateway/src/libs/socketEventsHandlers/feesSocketEventsHandler.js
@@ -7,7 +7,14 @@ function feesSocketEventsHandler(io, socket, socketOptions, FeesController) {
 
     const ProductFeesController = new FeesController();
     ProductFeesController.mountSocket(socketOptions);
-    ProductFeesController.createProductOrderPaymentResponse(io);    
+    ProductFeesController.createProductOrderPaymentResponse(io); 
+
+    
+    socket.on('getUserPayments', function(data) {
+        data.socketId = socket.id; 
+        ProductFeesController.getUserPayments(data);
+    });
+    ProductFeesController.getUserPaymentsResponse(io)
 }
 
 module.exports.feesSocketEventsHandler = feesSocketEventsHandler;
