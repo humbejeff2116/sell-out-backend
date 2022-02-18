d83fe4bd684b31b2f837e854b929b6ab1a80c1e9
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-18 20:10:08 +0000

    implement signup and login user controller methods (gateway)

--- API-gateway/src/controllers/HTTP_controllers/userController.js
+++ API-gateway/src/controllers/HTTP_controllers/userController.js
@@ -1,10 +1,38 @@
 
 
-const { getUserNotifications } = require('../../utils/http.services');
+const { getUserNotifications, signupUser, loginUser } = require('../../utils/http.services');
 
 
 function UserController() {}
 
+
+UserController.prototype.signupUser = async function(req, res) {
+
+    try {
+        const signupDetails = {
+            email: req.body.email,
+            fullName: req.body.fullname,
+            password: req.body.password
+        }
+        const signupedUserResponse  = await signupUser(signupDetails);
+        res.json(signupedUserResponse);   
+    } catch (err) {
+        console.error(err.stack)   
+    } 
+}
+
+UserController.prototype.loginUser = async function(req, res) {
+    try {
+        const loginDetails = {
+            email: req.body.email,
+            password: req.body.password
+        }
+        const loggedinUserResponse  = await loginUser(loginDetails);
+        res.json(loggedinUserResponse);   
+    } catch (err) {
+        console.error(err.stack)   
+    } 
+}
 UserController.prototype.getUserNotifications = async function(req, res) {
     try {
         const user = {
