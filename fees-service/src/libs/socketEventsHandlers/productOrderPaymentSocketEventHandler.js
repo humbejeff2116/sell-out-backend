98508be760800fc59132f6196a9d0227636a96fa
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-01 07:33:53 +0000

    add getUserPayments socket event and clean code (fees-service)

--- fees-service/src/libs/socketEventsHandlers/productOrderPaymentSocketEventHandler.js
+++ fees-service/src/libs/socketEventsHandlers/productOrderPaymentSocketEventHandler.js
@@ -1,20 +1,28 @@
 
-
-
-
-
-
 function productOrderPaymentSocketEventsHandler(io, socket, socketOptions, ProductOrderPaymentController) {
+
     const productOrderPaymentController = new ProductOrderPaymentController();
+
     productOrderPaymentController.mountSocket(socketOptions);
-    
+ 
     socket.on('createPayment', function(data) {
+
         productOrderPaymentController.createProductOrderPayment(io, socket, data);
+
     });
 
     socket.on('productDelivered', function(data) {
+
         productOrderPaymentController.paySellerAfterDelivery(io, socket, data);
+
     });
 
+    socket.on('getUserPayments', function(data) {
+
+        productOrderPaymentController.getUserPayments(io, socket, data);
+
+    })
+
 }
+
 module.exports.productOrderPaymentSocketEventsHandler = productOrderPaymentSocketEventsHandler;
