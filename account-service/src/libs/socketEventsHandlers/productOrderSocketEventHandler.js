21864727a67bf2840fbffc11642e2f1687d342ec
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-01 07:13:12 +0000

    clean productOrderSocketEventsHandler module (account-servive)

--- account-service/src/libs/socketEventsHandlers/productOrderSocketEventHandler.js
+++ account-service/src/libs/socketEventsHandlers/productOrderSocketEventHandler.js
@@ -1,27 +1,40 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function productOrderSocketEventsHandler(io, socket, socketOptions, ProductOrderController) {
    
     const productOrderController = new ProductOrderController();
+
     productOrderController.mountSocket(socketOptions);
-    socket.on('createOrder', function(data) {
-        return productOrderController.createOrder(data);
-    });
+
     socket.on('confirmDelivery', function(data) {
+
         return productOrderController.confirmDelivery(data);
+
     });
-   
+
+    socket.on('orderCreated', function(data) {
+
+        return productOrderController.notifySellerAboutOrder(data, io);
+
+    })
+
+    socket.on('productPaymentCreated', function(data) {
+
+        return productOrderController.notifySellerABoutPendingPayment(data, io);
+
+    })
+
+    socket.on('productDelivered', function(data) {
+
+        return productOrderController.addProductDeliveredNotification(data, io);
+
+    })
+    
+    socket.on('sellerPaymentFundsReleased', function(data) {
+
+        return productOrderController.addPaymentFundReleasedNotification(data, io);
+
+    })
+  
 }
+
 module.exports.productOrderSocketEventsHandler = productOrderSocketEventsHandler;
