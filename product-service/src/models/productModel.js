12682dbf4c41214d956f4e562f9e1a3ee6da292d
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-01 07:49:05 +0000

    implement full text search, addlike and removelike functionality on product model (product-service)

--- product-service/src/models/productModel.js
+++ product-service/src/models/productModel.js
@@ -1,147 +1,68 @@
 
 
-
-
-
-
-
-
 const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
 
+const CounterSchema = new mongoose.Schema({
 
+    _id: { type: String, required: true },
+    seq: { type: Number, default: 0 }
 
-
+});
 
 
 const ProductSchema =  mongoose.Schema({
+
     userName: { type: String, required: true },
     userId:{ type: String, required: true},
     userProfileImage: { type: String }, 
     userEmail: { type: String, required: true, },
     productName: { type: String, required: true },
     productCategory: { type: String, required: true },
-    productCountry: { type: String, required: true },
-    productState: { type: String, required: true },
     productUsage: { type: String },
     productCurrency: { type: String, required: true },
     productPrice: { type: String, required: true },
-    productContactNumber: { type: String, required: true },
-    sold: { type: Boolean },
+    numberSold: { type: Number },
     productImages: [{}],
-    stars: [{}],
-    unstars: [{}],
+    likes: [{}],
     comments: [{}],
-    interests: [{}],
     createdAt: { type: Date, default: Date.now }
+
 });
 
-ProductSchema.statics.getProducts = function( ) {
-    let products = this.find({});
-    return products;
-}
+ProductSchema.index({
 
-ProductSchema.statics.getUserProducts = function(userName) {
-    let products = this.find({userName});
-    return products;
-}
-ProductSchema.statics.getProductById = function(userId) {
-    let productOrService = this.findOne({ _id: userId });
-    return productOrService;
-}
+    '$**':'text'
 
-ProductSchema.methods.addStar = function(data) {
-    const star = parseInt(data.star);
-    const userName = data.user.userName;
-    const starData = {
-        star,
-        userName,
-        time: Date.now()
-    }
-    this.stars.push(starData);
-}
+})
 
-ProductSchema.methods.removeStar = function(data) {
-    const userName = data.user.userName;
-    function findUserPos(userName) {
-        for (let i = 0; i < this.stars.length; i++) {
-            if (this.stars[i].userName === userName ) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    let userPos = findUserPos(userName);
-    if (userPos > -1) {
-        return this.stars.splice(userPos, 1);
-    }
-}
+ProductSchema.pre('save', function(next) {
 
-ProductSchema.methods.review = function(data) {
-    const {user, reviewMessage} = data;
-   
-    const reviewData = {
-        userId: user.id,
-        userName: user.fullName,
-        userEmail: user.userEmail,
-        userProfilePicture: user.profileImage,
-        review: reviewMessage,
-        time: Date.now()
-    }
-    this.reviews.push(reviewData);
-}
-ProductSchema.methods.addInterest = function(data) {
-    const {user, productOrService } = data;
     const self = this;
-    const interestData = {
-        userId: user.id,
-        userEmail: user.userEmail,
-        productName: productOrService.productName,
-        productId: productOrService.productId,
-        time: Date.now()
-    }
-    function findUserPos(userEmail) {
-        for (let i = 0; i < self.interests.length; i++) {
-            if (self.interests[i].userEmail === userEmail ) {
-                return i;
-            }
+
+    Counter.findByIdAndUpdate(
+
+        { _id: "productId" },
+
+        { $inc: { seq: 1 } },
+
+        { new: true, upsert: true },
+
+        function(err, count) {
+
+            if (err) return next(err);
+
+            self.id = count.seq;
+
+            next();
         }
-        return -1;
-    }
-    const userPos = findUserPos(user.userEmail);
-    if(userPos > -1) {
-        return this.interests;
-    }
-    return this.interests.push(interestData);
-}
 
-ProductSchema.methods.removeInterest = function(data) {
-    try {
-        const {user, productOrService } = data;
-        const self = this;
-        function findUserPos(userEmail) {
-            for (let i = 0; i < self.interests.length; i++) {
-                if (self.interests[i].userEmail === userEmail ) {
-                    return i;
-                }
-            }
-            return -1;
-        }
-        const userPos = findUserPos(user.userEmail); 
-        if(userPos > -1) {
-            this.interests.splice(userPos, 1);
-            return this.interests;
-        }
-        return this.interests; 
+    )
 
-    } catch(e) {
-        console.error(e.stack);
-    }
-      
-}
-
-
+})
 
 ProductSchema.methods.setProductDetails = function(data = {}) {
+
     const {product, user} = data;
     this.userName = user.fullName;
     this.userEmail = user.userEmail;
@@ -150,13 +71,121 @@ ProductSchema.methods.setProductDetails = function(data = {}) {
     this.productName = product.productName;
     this.productCategory = product.productCategory;
     this.productImages = product.productImages;
-    this.productCountry = product.productCountry;
-    this.productState = product.productState;
     this.productUsage = product.productUsage;
     this.productCurrency = product.productCurrency;
     this.productPrice = product.productPrice;
-    this.productContactNumber = product.productContactNumber;
+
 }
 
+ProductSchema.methods.setProductDetailsHTTP = function(data = {}) {
+
+    this.userName = data.fullName;
+    this.userEmail = data.userEmail;
+    this.userId = data.id;
+    this.userProfileImage = data.profileImage;
+    this.productName = data.productName;
+    this.productCategory = data.productCategory;
+    this.productImages = data.productImages;
+    this.productUsage = data.productUsage;
+    this.productCurrency = data.productCurrency;
+    this.productPrice = data.productPrice;
+
+}
+
+ProductSchema.statics.getProducts = async function( ) {
+
+    let products = await this.find({});
+
+    return products;
+
+}
+
+ProductSchema.statics.getUserProducts = async function(userName) {
+
+    let products = await this.find({userName});
+
+    return products;
+
+}
+
+ProductSchema.statics.getProductById = async function(userId) {
+
+    let productOrService = await this.findOne({ _id: userId });
+
+    return productOrService;
+
+}
+
+ProductSchema.statics.addLike = async function({productId, like, user}) {
+
+    const product = await this.findOne({_id: productId});
+
+    if(userLikedProduct(user.userEmail, product.likes)) {
+
+        return ({status: 201, updated: false, data: null})
+
+    }
+
+    const starData = {
+        like: parseInt(like),
+        userName: user.userName,
+        userEmail: user.userEmail,
+        userId: user.id,
+        time: Date.now()
+    }
+
+    const addProductLike = await this.updateOne({ _id: productId }, { $push: { stars: starData } })
+
+    return ({ status: 201, updated: true, data: addProductLike })
+    
+}
+
+ProductSchema.statics.removeLike = async function({productId, like, user}) {
+
+    const product = await this.find({ _id: productId })
+
+    if (!userLikedProduct(user.userEmail, product.likes)) {
+
+        return ({status: 201, updated: false, data: null});
+
+    }
+
+    const removeProductLike = await this.updateOne({ _id: productId }, { $pull: { likes: { userEmail: user.userEmail } } });
+
+    return ({ status: 201, updated: true, data: removeProductLike });
+
+}
+
+ProductSchema.statics.searchProducts = async function(query) {
+
+   const products = await this.find({ $text: { $search: query } })
+
+   return products
+
+}
+
+function userLikedProduct(userEmail = "", likes = []) {
+
+    let i;
+
+    const likesLen = likes.length;
+
+    for (i = 0; i < likesLen; i++) {
+
+        if (likes[i].userEmail === userEmail ) {
+
+            return  true;
+
+        }
+
+    }
+
+    return false;
+
+}
+
+const Counter = mongoose.model('counter', CounterSchema);
+
 const Product = mongoose.model('Products', ProductSchema);
+
 module.exports = Product;
