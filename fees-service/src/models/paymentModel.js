295fa16b7ad529c39ffd44d99d538df55fa32069
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-01 07:29:51 +0000

    refactor payment model database queries (fees-service)

--- fees-service/src/models/paymentModel.js
+++ fees-service/src/models/paymentModel.js
@@ -1,16 +1,8 @@
 
-
-
-
-
-
 const mongoose = require('mongoose');
 
-
-
-
-
 const PaymentSchema =  mongoose.Schema({
+
     placedOrderId: { type: String, required: true },
     orderId: { type: String, required: true },
     orderTime: { type: String, required: true },
@@ -19,14 +11,17 @@ const PaymentSchema =  mongoose.Schema({
     sellerId: { type: String, required: true },
     buyerName: { type: String, required: true },
     buyerEmail: { type: String, required: true },
-    sellerId: { type: String, required: true },
+    buyerId: { type: String, required: true },
     paymentAmount: { type: String , required: true },
     paymentReleaseStatus: { type: String, default: "pending" },
     sellerRecievedPayment: { type: Boolean, default: false },
     productsSellerSold: [{}],
     createdAt: { type: Date, default: Date.now }
+
 });
+
 PaymentSchema.methods.setPaymentDetails = function(data, placedOrderId) {
+
     this.placedOrderId = placedOrderId;
     this.orderId = data.orderId;
     this.orderTime = data.orderTime;
@@ -38,43 +33,92 @@ PaymentSchema.methods.setPaymentDetails = function(data, placedOrderId) {
     this.buyerEmail = data.buyerEmail;
     this.paymentAmount = data.totalAmount;
     this.productsSellerSold = data.productsSellerSold; 
+
 }
+
 PaymentSchema.statics.updatePaymentStatus = async function({ 
+
     placedOrderId, 
     sellerEmail, 
     sellerId,  
     paymentReleaseStatus = "released", 
     sellerRecievedPayment = true 
+
 }) {
+
     const updatePaymentReleaseStatus = await this.updateOne(
+
         { $and: [{sellerEmail: sellerEmail}, { placedOrderId: placedOrderId }] },
         { "$set": {"paymentReleaseStatus":  paymentReleaseStatus, "sellerRecievedPayment": sellerRecievedPayment } }
+
     )
+
     return updatePaymentReleaseStatus;
+
 }
 
 PaymentSchema.statics.getSellerPayment = function({ placedOrderId, sellerEmail, sellerId }) {
+
     let payment = this.findOne({
+
         $and:[
             {sellerEmail: sellerEmail}, { placedOrderId: placedOrderId }
         ]
+
     });
+
     return payment;
+
 }
+
 PaymentSchema.statics.getSellerPayments = function(data) {
+
     const {userEmail} = data
+
     let payments = this.find({userEmail});
+
     return payments;
+
 }
+
 PaymentSchema.statics.getBuyerPayments = function(data) {
+
     const {userEmail} = data
+
     let payments = this.find({userEmail});
+
     return payments;
+
 }
 
+PaymentSchema.statics.getSellerPaymentsByEmailOrId = async function({sellerEmail, sellerId}) {
 
+    const payments = await this.find({
 
+        $or:[
+            {sellerEmail: sellerEmail}, { sellerId: sellerId }
+        ]
 
+    });
+
+    return payments;
+
+}
+
+PaymentSchema.statics.getBuyerPaymentsByEmailOrId = async function({buyerEmail, buyerId}) {
+
+    const payments = await this.find({
+
+        $or:[
+            {buyerEmail: buyerEmail}, { buyerId: buyerId }
+        ]
+
+    });
+
+    return payments;
+
+}
 
 const Payment = mongoose.model('payments', PaymentSchema);
+
 module.exports = Payment;
