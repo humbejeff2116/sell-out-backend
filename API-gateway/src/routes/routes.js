edb62036df8ce2c4bd145a9999092a470666c67e
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-13 19:25:18 +0000

    add update-user, search-products, reviews, stars, product-likes HTTP routes (gateway)

--- API-gateway/src/routes/routes.js
+++ API-gateway/src/routes/routes.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 const express = require('express')
 const path = require('path')
 const { multerUploads } = require('./Multer/multer');
@@ -10,16 +6,18 @@ const ProductControllerClass =  require('../controllers/HTTP_controllers/product
 const UserControllerClass =  require('../controllers/HTTP_controllers/userController');
 const ProductController = new ProductControllerClass();
 const UserController = new UserControllerClass();
-
-
 const router = express.Router();
-
 
 
 router.post('/product', ProductController.createProduct)
 router.get('/products', ProductController.getProducts)
+router.get('/search-products', ProductController.searchProducts)
+router.get('/reviews/:productId', ProductController.getProductComments)
 router.get('/notifications/:id/:userEmail', UserController.getUserNotifications)
+router.get('/stars/:userId', UserController.getUserStars)
+router.get('/product-likes/:productId', ProductController.getProductLikes)
 router.post('/signup', UserController.signupUser)
 router.post('/login', UserController.loginUser)
+router.post('/update-user', UserController.updateUser);
 
 module.exports = router;
