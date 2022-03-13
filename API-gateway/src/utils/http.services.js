7297d4bc9aef6d3aa9a41b3eca3f5e5f76ff9619
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-13 19:22:11 +0000

    add updateUser HTTP service and clean code (gateway)

--- API-gateway/src/utils/http.services.js
+++ API-gateway/src/utils/http.services.js
@@ -1,10 +1,5 @@
 
-
-
-
-
 const httpBase = require('./http.config');
-
 const LOGIN_SERVER_URI = `http://localhost:4001`;
 const PRODUCT_SERVER_URI = `http://localhost:4003`;
 const DATA_MERGER_SERVER_URI = `http://localhost:4002`;
@@ -16,39 +11,83 @@ const dataMergerServerURL = httpBase(DATA_MERGER_SERVER_URI);
 module.exports = {
 
     signupUser: async function (signupDetails) {
-        // return loginServerURL.get(`/products`);
+
         const singupResponse = await loginServerURL.post(`/signup`, signupDetails );
-        const data = singupResponse.data;
-        return data;
+       
+        return singupResponse.data;
+
     },
 
     loginUser: async function (loginDetails) {
-        // return loginServerURL.get(`/products`);
+
         const loginResponse = await loginServerURL.post(`/login`, loginDetails);
-        const data = loginResponse.data;
-        return data;
+
+        return loginResponse.data;
+
     },
 
     getProducts: async function () {
-        // return loginServerURL.get(`/products`);
-        const productsResponse = await dataMergerServerURL.get(`/products`);
-        const data = productsResponse.data;
-        return data;
+
+        const productsResponse = await productServerURL.get(`/products`);
+
+        return productsResponse.data;
+        
     },
     createProduct: async function (data, headers) {
+
         const createdProductResponse = await productServerURL.post(`/product`, data, { headers: {...headers}});
-        const createdProductResponseData = createdProductResponse.data;
-        return createdProductResponseData;
+
+        return createdProductResponse.data;
+
     },
     authenticateUser: async function (user) {
+
         const response = await loginServerURL.post(`/authenticate-user/`, user);
-        const data = response.data;
-        return data;
+
+        return response.data;
+
     },
     getUserNotifications: async function (user) {
+
         const response = await loginServerURL.get(`/notifications/${user.id}/${user.userEmail}`);
-        const data = response.data;
-        return data
+
+        return response.data;
+
     },
+    searchProducts : async function (query) {
+
+        const searchProductsResponse = await productServerURL.post(`/searc-products?q=${query}`);
+ 
+        return searchProductsResponse.data;
+
+    },
+    getProductComments : async function(productId) {
+
+        const productCommentsResponse = await productServerURL.get(`/reviews/${productId}`);
+
+        return productCommentsResponse.data;
+        
+    },
+    getUserStars : async function(userId) {
+
+        const userStarsResponse = await loginServerURL.get(`/stars/${userId}`);
+        
+        return userStarsResponse.data
+
+    },
+     getProductLikes : async function(productId) {
+
+        const productLikesResponse = await productServerURL.get(`/product-likes/${productId}`);
+        
+        return productLikesResponse.data
+
+    },
+    updateUser: async function(data, headers) {
+
+        const updateUserResponse = await loginServerURL.post(`/update-user`, data, { headers: {...headers}});
+
+        return updateUserResponse.data;
+
+    }
 
 }
