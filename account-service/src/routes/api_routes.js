d9d74b1a92f5f88521582f267aa509b08cda1bf9
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-15 09:05:21 +0000

    add stars, signup, login and delivery-regions routes (account-service)

--- account-service/src/routes/api_routes.js
+++ account-service/src/routes/api_routes.js
@@ -1,13 +1,4 @@
 
-
-
-
-
-
-
-
-
-
 const express = require('express');
 const { multerUploads } = require('./Multer/multer');
 const ProductControllerClass = require('../controllers/HTTP_controllers/productController');
@@ -17,8 +8,12 @@ const UserController = new UserControllerClass();
 
 const router = express.Router();
 router.post('/authenticate-user', UserController.authenticateUser);
-router.post('/update-user', UserController.updateUser);
+router.post('/update-user', multerUploads, UserController.updateUser);
 router.get('/products', ProductController.getProducts);
 router.get('/users', UserController.getUsers);
 router.get('/notifications/:id/:userEmail', UserController.getUserNotifications)
+router.get('/stars/:userId', UserController.getUserStars)
+router.post('/signup', UserController.signupUser);
+router.post('/login', UserController.loginUser);
+router.post('/delivery-regions/:userId', UserController.getDeliveryRegions);
 module.exports = router;
