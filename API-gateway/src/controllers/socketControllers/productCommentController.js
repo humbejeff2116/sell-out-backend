78b7ad8dc3c308a68bf333e311304c55cf73cdee
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-26 21:39:43 +0000

    clean socket productCommentController module (gateway)

--- API-gateway/src/controllers/socketControllers/productCommentController.js
+++ API-gateway/src/controllers/socketControllers/productCommentController.js
@@ -5,9 +5,11 @@
  * @module ProductCommentController
  */
  function ProductCommentController() {
-    this.userClient;
+
     this.productClient;
+    
     this.gatewayServerSocket;
+
 }
 
 /**
@@ -20,10 +22,13 @@
  * 
  */
  ProductCommentController.prototype.mountSocket = function({ userClient, productClient, gatewayServerSocket}) {
-    this.userClient = userClient ? userClient : null;
+
     this.productClient = productClient ? productClient : null;
+
     this.gatewayServerSocket = gatewayServerSocket ? gatewayServerSocket : null;
+
     return this;
+
 }
 
 /** 
@@ -35,112 +40,139 @@
  ** Sends back response to frontend 
  * @param {object} data - the user data collected from the front end which includes the user and product or service to review
  */
- ProductCommentController.prototype.reviewProductOrService = function(data = {}) {
+ ProductCommentController.prototype.reviewProduct = function(io, socket, data = {}) {
+
     const { user } = data;
-    if(!user) {
+
+    if (!user) {
+
         const response = {
             error:true,
             status:403,
             message:"you must log in to leave a review"
         }
+
         return this.gatewayServerSocket.emit('unRegisteredUser', response);
+
     }
-    this.productClient.emit('reviewProductOrService', data);   
-}
 
-ProductCommentController.prototype.reviewProductOrServiceResponse = function(io) {
-    const self = this;
-    this.productClient.on('reviewProductUserError', function(response) {
-        const {socketId} = response;
-        io.to(socketId).emit('reviewProductUserError', response);
-        console.log(response);
-    })
-    this.productClient.on('reviewProductOrServiceError', function(response) {
-        const {socketId} = response;
-        io.to(socketId).emit('reviewProductOrServiceError', response);
-        console.log(response);
+    this.productClient.emit('reviewProduct', data, (response) => {
+        
+        if (response.error ) {
+
+            const { socketId, ...rest } = response;
+
+            io.to(socketId).emit('reviewProductError', response);
+
+            return
+        }
+
+        io.emit('reviewDataChange');
+
     });
-    this.productClient.on('reviewProductOrServiceSuccess', function(response) {
-        io.sockets.emit('productDataChange');
-    });  
+
 }
 
-ProductCommentController.prototype.replyReviewProductOrService = function(data = {}) {
+ProductCommentController.prototype.replyReviewProduct = function(io, socket, data = {}) {
+
     const { user } = data;
-    if(!user) {
+
+    if (!user) {
+
         const response = {
             error:true,
             status:403,
             message:"you must log in to reply a review"
         }
-        return this.gatewayServerSocket.emit('unRegisteredUser', response);
-    }
-    this.productClient.emit('replyReviewProductOrService', data);   
-}
 
-ProductCommentController.prototype.replyReviewProductOrServiceResponse = function(io) {
-    const self = this;
-    
-    this.productClient.on('replyReviewProductOrServiceError', function(response) {
-        const {socketId} = response;
-        io.to(socketId).emit('replyReviewProductOrServiceError', response);
-        console.log(response);
-    })
-    this.productClient.on('replyReviewProductOrServiceSuccess', function(response) {
-        console.log('response on review is', response)
-        io.sockets.emit('productDataChange');
-    });   
+        return this.gatewayServerSocket.emit('unRegisteredUser', response);
+
+    }
+
+    this.productClient.emit('replyReviewProduct', data, (response) => {
+
+        if (response.error ) {
+
+            const { socketId, ...rest } = response;
+
+            io.to(socketId).emit('replyReviewProductError', response);
+
+            return
+        }
+
+        io.emit('reviewDataChange');
+
+    });
+
 }
 
 // like comment
-ProductCommentController.prototype.likeComment = function(data = {}) {
+ProductCommentController.prototype.likeReview = function(io, socket, data = {}) {
+
     const { user } = data;
+
     if (!user) {
+
         const response = {
             error:true,
             status:403,
             message:"you must log in to like a comment"
         }
+
         return this.gatewayServerSocket.emit('unRegisteredUser', response);
     }
-    this.productClient.emit('likeComment', data);   
+
+    this.productClient.emit('likeReview', data, (response) => {
+
+        if (response.error ) {
+
+            const { socketId, ...rest } = response;
+
+            io.to(socketId).emit('likeReviewError', response);
+
+            return
+        }
+
+        io.emit('reviewDataChange');
+
+    });
+       
 }
 
-ProductCommentController.prototype.likeCommentResponse = function(io) {
-    
-    this.productClient.on('likeCommentError', function(response) {
-        const {socketId} = response;
-        io.to(socketId).emit('likeCommentError', response);
-    })
-    this.productClient.on('likeCommentSuccess', function(response) {
-        io.sockets.emit('productDataChange', response); 
-    }); 
-}
 
 // unlike comment
-ProductCommentController.prototype.unLikeComment = function(data = {}) {
+ProductCommentController.prototype.dislikeReview = function(io, socket, data = {}) {
+
     const { user } = data;
-    if(!user) {
+
+    if (!user) {
+
         const response = {
             error:true,
             status:403,
             message:"you must log in to dislike a comment"
         }
-        return this.gatewayServerSocket.emit('unRegisteredUser', response);
-    }
-    this.productClient.emit('unLikeComment', data);   
-}
 
-ProductCommentController.prototype.unLikeCommentResponse = function(io) {
-    
-    this.productClient.on('unLikeCommentError', function(response) {
-        const {socketId} = response;
-        io.to(socketId).emit('unLikeCommentError', response);
-    })
-    this.productClient.on('unLikeCommentSuccess', function(response) {
-        const {socketId} = response;
-        io.sockets.emit('productDataChange', response); 
-    });   
+        return this.gatewayServerSocket.emit('unRegisteredUser', response);
+
+    }
+
+    this.productClient.emit('dislikeReview', data, (response) => {
+
+        if (response.error ) {
+
+            const { socketId, ...rest } = response;
+
+            io.to(socketId).emit('dislikeReviewError', response);
+
+            return;
+
+        }
+
+        io.emit('reviewDataChange');
+
+    });
+
 }
 
 module.exports = ProductCommentController;
