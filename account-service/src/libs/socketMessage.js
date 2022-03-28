004d282413079a2d916a6606d2741db9eb151ba9
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-28 22:11:04 +0100

    clean up socketMessage module (account service)

--- account-service/src/libs/socketMessage.js
+++ account-service/src/libs/socketMessage.js
@@ -1,20 +1,23 @@
 
-
-
-
-
-
 const message = (function() {
+
     return({
         socketConnectionMessage,
         socketDisconnetionMessage
     });
+
     function socketConnectionMessage(socket) {
+
         console.log("Gateway client connected --> " + socket.id);
+
     }
+
     function socketDisconnetionMessage(socket) {
-        console.log("Gateway client disconnected --> " + socket.id);
+
+        console.log("socket disconnected --> " + socket.id);
+
     }
+    
 })()
 
 module.exports = message;
