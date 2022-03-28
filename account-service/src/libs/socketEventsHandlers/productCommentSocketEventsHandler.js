0b6e1d24a29b9fc5faa285f9d39690da68dfe26e
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-28 22:09:19 +0100

    clean productCommentSocketEventsHandler and userSocketEventsHandler modules (account service)

--- account-service/src/libs/socketEventsHandlers/productCommentSocketEventsHandler.js
+++ account-service/src/libs/socketEventsHandlers/productCommentSocketEventsHandler.js
@@ -1,24 +1,34 @@
 
-
 function productCommentSocketEventsHandler(io, socket, socketOptions, ProductCommentController) {
    
     const productCommentController = new ProductCommentController();
+
     productCommentController.mountSocket(socketOptions);
-    socket.on('reviewProductOrServiceSuccess', function(data) {
-        productCommentController.addReviewProductNotification(data, io);
+
+    socket.on('reviewProductSuccess', function(data) {
+
+        productCommentController.addReviewProductNotification(data, io, socket);
+
     });
    
-    socket.on('replyReviewProductOrServiceSuccess', function(data) {
-        productCommentController.addReplyReviewProductNotification(data, io);   
+    socket.on('replyReviewProductSuccess', function(data) {
+
+        productCommentController.addReplyReviewProductNotification(data, io, socket);
+
     });
 
     socket.on('likeCommentSuccess', function(data) {
-        productCommentController.addLikeCommentNotification(data, io);   
+
+        productCommentController.addLikeCommentNotification(data, io, socket); 
+
     });
    
     socket.on('unLikeCommentSuccess', function(data) {
-        productCommentController.addDislikeCommentNotification(data, io);   
+
+        productCommentController.addDislikeCommentNotification(data, io, socket);
+
     });
    
 }
+
 module.exports.productCommentSocketEventsHandler = productCommentSocketEventsHandler;
--- account-service/src/libs/socketEventsHandlers/userSocketEventsHandler.js
+++ account-service/src/libs/socketEventsHandlers/userSocketEventsHandler.js
@@ -1,45 +1,64 @@
 
-
-
-
-
-
-
-
 function userSocketEventsHandler(io, socket, socketOptions, UserController) {
+
     const userController = new UserController();
+
     userController.mountSocket(socketOptions)
-   socket.on('signUp', function(data) {  
-       return userController.signUp(data);
-   })
-   socket.on('login', function(data) {
-       userController.login(data);
-   }) 
-   socket.on('getUserById', function(data) {
-      return userController.getUserById(data);
-   })
-   socket.on('getAllUsers', function(data) {
-    return userController.getAllUsers(io, socket, data);
- })
-   socket.on('starUser', function(data) {
-       userController.starUser(data)
-   })
+
+    socket.on('starUser', function(data, callback) {
+       
+        userController.starUser(io, socket, data, callback)
+
+    })
     // get seller stars
     socket.on('getInitialStarData', function(data) {
+
        userController.getUserStars(data);
-   });
-   socket.on('getNotifications', function(data) {
+
+    });
+
+    socket.on('getNotifications', function(data) {
+
        userController.getNotifications(data);
-   });
-   socket.on('seenNotifications', function(data) {
+
+    });
+
+    socket.on('seenNotifications', function(data) {
+
        userController.seenNotifications(io, socket, data);
-   });
-   socket.on('getInterests', function(data) {
-       userController.getUserInterests(data);
-   });
-   socket.on('getConfirmations', function(data) {
-       userController.getUserConfirmations(data);
-   });
-    
+
+    });
+
+    socket.on('getInterests', function(data) {
+
+        userController.getUserInterests(data);
+
+    });
+
+    socket.on('getConfirmations', function(data) {
+
+        userController.getUserConfirmations(data);
+
+    });
+
+    socket.on('getUserPreviousSearches', function(data, callback) {
+
+        userController.getUserPreviousSearches(io, socket,data, callback);
+
+    });
+
+    socket.on('updateUserSearchData', function(data) {
+
+        userController.updateUserSearchData(io, socket,data);
+
+    });
+
+    socket.on('removeUserSearch', function(data) {
+
+        userController.removeUserSearch(io, socket,data);
+
+    });
+   
 }
+
 module.exports.userSocketEventsHandler = userSocketEventsHandler;
