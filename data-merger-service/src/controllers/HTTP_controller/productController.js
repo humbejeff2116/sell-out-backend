de4875caeb7e5ee6d0c0a6aea7383ef2e0bf307d
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-29 11:03:16 +0100

    modify get poducts functionality (data-merger service)

--- data-merger-service/src/controllers/HTTP_controller/productController.js
+++ data-merger-service/src/controllers/HTTP_controller/productController.js
@@ -1,35 +1,22 @@
 
+const { getProducts } = require('../../utils/http.services');
 
-
-const { getProducts, getUsers } = require('../../utils/http.services');
-
-function ProductsController() {
-    
-}
+function ProductsController() { }
 
 ProductsController.prototype.getProducts = async function(req, res) {
+
     try {
+
         const products = await getProducts();
-        const users = await getUsers();
-        return setStarsAndSendProductsToGateway(users, products, res);
-        
-        async function setStarsAndSendProductsToGateway(users, products, res) {
-            const usersStars =  users.map(user => ({ 
-                userEmail: user.userEmail,
-                starsUserRecieved: user.starsUserRecieved
-            }));
-            for (i = 0; i < products.length; i++) {
-                for (j = 0; j < usersStars.length; j++) {
-                    if (products[i].userEmail === usersStars[j].userEmail) {
-                        products[i].starsUserRecieved = usersStars[j].starsUserRecieved;
-                    }
-                }
-            }
-            return  res.status(200).json(products);   
-        }
-        
+       
+        res.status(200).json(products);
+   
     } catch (err) {
-        console.error(err.stack);   
-    }  
+
+        console.error(err); 
+
+    }
+
 }
+
 module.exports = ProductsController;
