366cd7ce9348249f62b638e1cef9cab9d864490f
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-29 10:45:28 +0100

    add gatewayClient property to socketOptions object (account service)

--- account-service/src/utils/socketConnections.js
+++ account-service/src/utils/socketConnections.js
@@ -1,13 +1,25 @@
 
 const socketOptions = {
     productClient: require('socket.io-client')('http://localhost:4003'),
-    // gatewayClient: require('socket.io-client')('http://localhost:4000'),
+    gatewayClient: require('socket.io-client')('http://localhost:4000'),
     serverSocket: null,
     allConnectedSockects: null,
 }
 
 socketOptions.productClient.on('connect', function() {
+
     socketOptions.productClient.sendBuffer = [];
-    console.log("product client has connected inside account service")
+    
+    console.log(`${"account service"} ---> ${"product service"} status: ${"connected"}`)
+
 });
+
+socketOptions.gatewayClient.on('connect', function() {
+
+    socketOptions.productClient.sendBuffer = [];
+
+    console.log(`${"account service"} ---> ${"qateway service"} status: ${"connected"}`)
+
+});
+
  module.exports = socketOptions
