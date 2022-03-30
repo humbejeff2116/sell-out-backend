86ee26c570e23d8d436e31af274f9afedd6b2c7d
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-30 10:03:37 +0100

    add getProductComments, getProductLikes, sendJSONError methods, refactor createProduct method on HTTP productController class (product service)

--- product-service/src/controllers/HTTP_controller/productController.js
+++ product-service/src/controllers/HTTP_controller/productController.js
@@ -1,115 +1,142 @@
 
-
-
-
 const Product = require('../../models/productModel');
 const Service = require('../../models/serviceModel');
 const Comment = require('../../models/commentModel');
 const { authenticateUser } = require('../../utils/http.services');
 const elasticSearch = require('elasticsearch');
 const saveProductToElasticSearch = require('../../utils/elasticSearch');
-const { contentSecurityPolicy } = require('helmet');
 const { imageDataUri } = require('../../routes/Multer/multer');
 const config = require('../../Config/config');
 const cloudinary = require('cloudinary').v2;
+
 cloudinary.config({ 
     cloud_name:config.cloudinary.cloudName, 
     api_key: config.cloudinary.apiKey, 
     api_secret:config.cloudinary.secret 
 });
 
+class ProductController {
 
-function ProductController() {
-    
-}
+    constructor() {
 
+       this.createProduct = this.createProduct.bind(this); 
 
+       this.getProducts = this.getProducts.bind(this);
 
-ProductController.prototype.createProduct = async function(req, res) {
-    // TODO... save product or service to elastice search data base
+       this.searchProducts = this.searchProducts.bind(this);
 
+       this.getProductComments = this.getProductComments.bind(this);
 
-    const userId = req.body.id;
-    const userName = req.body.fullName;
-    const userEmail = req.body.userEmail;
-    const userProfileImage = req.body.profileImage;
-    const productName = req.body.productName;
-    const productCategory = req.body.productCategory;
-    const productCountry = req.body.productCountry;
-    const productState = req.body.productState;
-    const productUsage = req.body.productUsage;
-    const productCurrency = req.body.productCurrenc
-    const productPrice = req.body.productPrice;
-    const productContactNumber = req.body.productContactNumber;
-    const productImages = req.fles ? imageDataUri(req) : [];
-    const userData = {
-        userId,
-        userName,
-        userEmail
+       this.getProductLikes = this.getProductLikes.bind(this);
+
+       this.sendJSONError = this.sendJSONError.bind(this);
+
+    }
+   
+    async createProduct (req, res) {
+
+        // const userId = req.body.id;
+        // const userName = req.body.fullName;
+        // const userEmail = req.body.userEmail;
+        // const userProfileImage = req.body.profileImage;
+        // const productName = req.body.productName;
+        // const productCategory = req.body.productCategory;
+        // const productCountry = req.body.productCountry;
+        // const productState = req.body.productState;
+        // const productUsage = req.body.productUsage;
+        // const productCurrency = req.body.productCurrenc
+        // const productPrice = req.body.productPrice;
+        // const productContactNumber = req.body.productContactNumber;
+        const productImages = req.files ? imageDataUri(req) : [];
+
+        const userData = {
+            userId,
+            userName,
+            userEmail
+        };
+
+        if (productImages.length > 0) {
+
+            const multipleUpload = new Promise(async (resolve, reject) => {
+
+                const uploadedImages = [];
+
+                for (let i = 0; i < productImages.length; i++) {
+
+                    try {
+
+                        const uploadedImage = await cloudinary.uploader.upload(productImages[i].content);
+
+                        uploadedImages[i] = { src: uploadedImage.url };
+
+                    } catch (err) {
+
+                        reject(err);
+
+                    }
+
+                }
+
+                resolve(uploadedImages);
+
+            });
+
+            multipleUpload.then( async images => {
+
+                req.body.productImages = images;
+
+                const product = new Product();
+
+                product.setProductDetails(req.body);
+
+                try {
+
+                    const savedProduct = await product.save();
+
+                    res.status(201).json({ status: 201, message: 'product uploaded sucessfully', data: savedProduct });
+
+                } catch (err) {
+
+                    console.error(err);
+
+                    res.json({ status: 500, error: true, message: 'An error occured while uploading product to database' });
+
+                    res.status(500);
+
+                }
+
+            }).catch(e => console.error(e.stack));
+
+        }
+
     }
 
-    if (productImages.length > 0) {
-        const multipleUpload =  new Promise(async (resolve, reject) => {
-            const uploadedImages = [];
-
-            for (let i = 0; i < productImages.length; i++) {
-                await cloudinary.uploader.upload(productImages[i].content)
-                .then(image => uploadedImages[i] = {src: image.url})
-                .catch(err =>  reject(err));
-            }
-            resolve(uploadedImages);      
-        });
+    async getProducts(req, res) {
         
-        multipleUpload.then(images => {
-            const data = {
-                userId,
-                userName,
-                userEmail,
-                userProfileImage ,
-                productName,
-                productCategory,
-                productImages: images,
-                productCountry,
-                productState,
-                productUsage,
-                productCurrency,
-                productPrice,
-                productContactNumber
-            }
-            const product = new Product()
-            product.setProductDetails(data);
-            product.save()
-            .then(data => {
-                res.status(201).json({ status: 201, message: 'product uploaded sucessfully'});
-            })
-            .catch(err => {
-                console.error(err.stack);           
-                res.json({ status: 500, message: 'An error occured while uploading product to database' });
-                res.status(500);
-            });
-        
-        })
-        .catch( e => console.error(e.stack))
-    }        
-}
+        try {
 
-ProductController.prototype.getProducts = async function(req, res) {
-    const products = await Product.getProducts();
-    const comments = await Comment.getAllComments();
-    if(products && comments) {
-        for (let i = 0; i < products.length; i++) {
-            for (let j = 0; j < comments.length; j++) {
-                if(products[i]._id.toString() === comments[j].productOrServiceId.toString()) {
-                    products[i].comments.push(comments[j])
-                }
+            const products = await Product.getProducts(); 
+
+            if (!products || products.length < 1) {
+
+                const response = {
+                    satus: 200,
+                    error: false,
+                    data: null,
+                    message: "no products found"
+                };
+
+                return res.status(200).json(response);
+
             }
-        }
-        const productsResponse = products.map( product => {
+
+            const productsResponse = products.map(product => {
+
                 return ({
                     userId: product.userId,
                     userName: product.userName,
                     userEmail: product.userEmail,
-                    userProfilePicture: product.userProfilePicture,
+                    userProfileImage: product.userProfileImage,
                     productId: product._id,
                     productName: product.productName,
                     productCategory: product.productCategory,
@@ -124,15 +151,182 @@ ProductController.prototype.getProducts = async function(req, res) {
                     unstars: product.unstars,
                     comments: product.comments,
                     interests: product.interests
+                });
+
             });
-        });
-        const response = {
-            satus:200,
-            data: productsResponse,
-            message:"gotten products data successfully"
+            
+            const response = {
+                satus: 200,
+                error: false,
+                data: productsResponse,
+                message: "gotten products data successfully"
+            };
+
+            return res.status(200).json(response);
+   
+        } catch (err) {
+
+            console.error(err);
+
         }
-        return res.status(200).json(response); 
+
     }
+
+    async searchProducts(req, res) {
+
+        let response;
+
+        try {
+
+            const query = req.query.q;
+
+            // TODO... send query to account service and update user search model propety
+
+            console.log("search query is", query);
+
+            const searchProducts = await Product.searchProducts(query);
+
+            console.log("search products", searchProducts)
+
+            if (searchProducts.length < 1) {
+
+                response = {
+                    status: 400,
+                    error: false,
+                    message: 'No product matches your search',
+                    data: []
+                }
+
+                res.json(response);
+
+                return res.status(400);
+
+            }
+
+            response = {
+                status: 200,
+                error: false,
+                message: "gotten products data successfully",
+                data: searchProducts
+            }
+
+            return res.status(200).json(response);
+
+        } catch (err) {
+
+            console.error(err);
+
+            res.json({ status: 500, error: true, message: 'an error occured while getting products' });
+
+            return res.status(500);
+
+        }
+
+    }
+
+    async getProductComments(req, res) {
+
+        const productId = req.params.productId;
+
+        console.log("product id",productId)
+
+        console.log("getting  product comments ---productService---")
+
+        let response;
+
+        try {
+
+            const productComments = await Comment.getProductComments(productId)
+
+            if (!productComments || productComments.length < 1) {
+
+                response = {
+                    status: 200,
+                    message: "no product comments found",
+                    data: []
+                }
+
+                return res.status(200).json(response)
+
+            }
+
+            response = {
+                status: 200,
+                message: "product comments found",
+                data: productComments
+            }
+
+            return res.status(200).json(response)
+
+        } catch (err) {
+
+            console.error(err)
+            
+        }
+
+    }
+
+    async getProductLikes(req, res) {
+
+        const productId = req.params.productId;
+
+        console.log("product id",productId)
+
+        console.log("getting  product likes ---productService---")
+
+        let response;
+
+        try {
+
+            const product = await Product.getProductById(productId)
+
+            if (!product) {
+
+                response = {
+                    error: true,
+                    status: 400,
+                    message: "product not found", 
+                }
+
+                res.json(response)
+
+                return res.status(200)
+
+            }
+            
+            const productLikes = product.likes;
+
+            response = {
+                status: 200,
+                message: "product likes gotten successfully",
+                data: productLikes
+            }
+
+            return res.status(200).json(response)
+
+        } catch (err) {
+
+            console.error(err)
+
+            this.sendJSONError(res, 500, true, "An error occured while getting product likes")
+            
+        }
+
+    }
+
+    sendJSONError(res, status, error, message) {
+
+        const response = {
+            status,
+            error, 
+            message, 
+        }
+    
+        res.json(response) 
+    
+        return res.status(status)
+    }
+
 }
 
-module.exports = ProductController;
+module.exports = new ProductController();
