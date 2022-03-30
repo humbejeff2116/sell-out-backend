521e5168a06703594f64b274ed9bb0a270637134
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-30 09:40:56 +0100

    refactor orderSocketEventsHandler module functionality (order service)

--- order-service/src/libs/socketEventsHandlers/orderSocketEventHandler.js
+++ order-service/src/libs/socketEventsHandlers/orderSocketEventHandler.js
@@ -1,26 +1,28 @@
 
-
-
-
-
-
-
-
 function orderSocketEventsHandler(io, socket, socketOptions, ProductOrderController) {
+
     const productOrderController = new ProductOrderController();
+
     productOrderController.mountSocket(socketOptions);
-    socket.on('createOrder', function(data) {
-        productOrderController.createOrder(io, socket, data);
-    });
-    productOrderController.createOrderResponse(io, socket);
-    // get product orders
-    socket.on('getUserProductOrders', function(data) {
-        productOrderController.getUserProductOrders(io, socket, data);
+
+    socket.on('createOrder', function(data, callback= f =>f) {
+
+        productOrderController.createOrder(io, socket, data, callback= f =>f);
+
     });
 
-    socket.on('confirmDelivery', function(data) {
-        productOrderController.confirmDelivery(io, socket, data);
+    socket.on('getUserProductOrders', function(data, callback= f =>f) {
+
+        productOrderController.getUserProductOrders(io, socket, data, callback= f =>f);
+
     });
-    productOrderController.confirmDeliveryResponse(io, socket)
+
+    socket.on('confirmDelivery', function(data, callback= f =>f) {
+
+        productOrderController.confirmDelivery(io, socket, data, callback= f =>f);
+
+    });
+
 }
+
 module.exports.orderSocketEventsHandler = orderSocketEventsHandler;
