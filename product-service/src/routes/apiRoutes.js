18f2636b699d2b9cb9f1cb31937a6b53be1b0fa3
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-31 19:47:02 +0100

    add productReviews, search-products, products-likes routes (product service)

--- product-service/src/routes/apiRoutes.js
+++ product-service/src/routes/apiRoutes.js
@@ -1,21 +1,14 @@
 
-
-
-
-
-
-
-
 const express = require('express');
-const productControllerClass = require('../controllers/HTTP_controller/productController');
-const ProductController = new productControllerClass();
+const  ProductController   = require('../controllers/HTTP_controller/productController');
 const { multerUploads } = require('./Multer/multer');
 const router = express.Router();
 
 
-
-
 router.post('/product', multerUploads, ProductController.createProduct);
 router.get('/products', ProductController.getProducts);
+router.get('/reviews/:productId', ProductController.getProductComments);
+router.get('/search-products', ProductController.searchProducts);
+router.get('/product-likes/:productId', ProductController.getProductLikes);
 
 module.exports = router;
