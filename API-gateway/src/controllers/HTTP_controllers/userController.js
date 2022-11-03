431a00f9e32b34cc00fc505f9c300b76edc0d8b9
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-03 21:51:10 +0000

    extract sendJSONError function to libs and clean userController module code (api-gateway)

--- API-gateway/src/controllers/HTTP_controllers/userController.js
+++ API-gateway/src/controllers/HTTP_controllers/userController.js
@@ -1,7 +1,6 @@
 
 const formidable = require('formidable');
-const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 var FormData = require('form-data');
 const { 
     getUserNotifications, 
@@ -13,265 +12,166 @@ const {
     getDeliveryRegions,
     getPreviousSearches 
 } = require('../../utils/http.services');
+const  { 
+    sendJSONError 
+} = require('../../libs/responses');
 
 
 function UserController() {}
 
-
 UserController.prototype.signupUser = async function(req, res) {
+    const signupDetails = {
+        email: req.body.email,
+        fullName: req.body.fullname,
+        password: req.body.password
+    }
 
     try {
-
-        const signupDetails = {
-            email: req.body.email,
-            fullName: req.body.fullname,
-            password: req.body.password
-        }
-
         const signupedUserResponse  = await signupUser(signupDetails);
 
         res.status(200).json(signupedUserResponse); 
-
     } catch (err) {
-
-        console.error(err.stack) 
-
+        console.error(err) ;
+        sendJSONError(res, 500, true, "error occured while creating account", err);
     }
-
 }
 
 UserController.prototype.loginUser = async function(req, res) {
+    const loginDetails = {
+        email: req.body.email,
+        password: req.body.password
+    }
 
     try {
-
-        const loginDetails = {
-            email: req.body.email,
-            password: req.body.password
-        }
-
         const loggedinUserResponse  = await loginUser(loginDetails);
 
         res.status(200).json(loggedinUserResponse); 
-
     } catch (err) {
-
-        console.error(err)
-
+        console.error(err);
+        sendJSONError(res, 500, true, "error occured while signing in", err);
     } 
-
 }
 
 UserController.prototype.updateUser = async function(req, res) {
+    const socketInstance = req.app.get("socketInstance") ? req.app.get("socketInstance") : null;
+    const socketId = socketInstance ? socketInstance.socket.id : null;
+    const io = socketInstance ? socketInstance.io : null;
+    const formData = new FormData();
+    const formHeaders = formData.getHeaders();
 
     try {
-
-        const socketInstance = req.app.get("socketInstance") ? req.app.get("socketInstance") : null ;
-
-        const socketId = socketInstance ? socketInstance.socket.id : null;
-
-        const io = socketInstance ? socketInstance.io : null ;
-
         const form = formidable({ multiples: true, });
 
-        const formData = new FormData();
-
-        const formHeaders = formData.getHeaders();
-
         form.parse(req, async (err, fields, files) => {
-
             try {
-
                 if (err) {
-
                     console.error(err);
-
-                    res.status(400).json("An error occured while updating user details");
-
-                    return;
-
+                    throw new Error(err);  
                 }
 
-                const userData = {
-                    id: fields.id,
-                    userName: fields.fullName,
-                    userEmail: fields.userEmail
-                }
-
-                // append create product data from client to server form-data
-                for (let keys in fields) {
-
-                    formData.append(keys, fields[keys])
-
-                }
-
-                for (let keys in files) {
-
-                    formData.append(keys, fs.createReadStream(files[keys].path), {
-
-                        filename: files[keys].name,
-
-                    });
-
-                }
-            
-                const authenticatedUserData = await authenticateUser(userData);
+                const fieldsEntries = Object.entries((fields));
+                const filesEntries = Object.entries((files));
+                const authenticatedUserData = await authenticateUser(JSON.parse(fields.user));
 
                 if (!authenticatedUserData.userExist) {
-
                     return res.status(401).json(authenticatedUserData);
-
                 }
-
-                // send product details to product server
-                sendUpdateUserData(formData, formHeaders, res);
-            
-                
-            } catch (err) {
-
-                console.error(err); 
-
-            } 
-
-        });
-
-        async function sendUpdateUserData(data, headers, res) {
-
-            try {
-
-                const updateUserResponse = await updateUser(data, headers)
-
-                if (updateUserResponse.error) {
-
-                    return  sendJSONError(res, 500, true, "error occured while updating user")
-        
+                // append user data from client to server form-data
+                for (let [key, value] of fieldsEntries) {
+                    formData.append(key, value);
                 }
-                
-                if (io) {
-
-                    io.sockets.emit('userDataChange');
-
-                    return res.status(200).json(updateUserResponse);
-
-                }
-
-                return res.status(200).json(updateUserResponse);
-
-            } catch (err) {
-
-                console.error(err) 
-
-            }
-
-        }
     
+                for (let [key, value] of filesEntries) {
+                    formData.append(key, fs.createReadStream(value.path), {
+                        filename: value.name,
+                    });
+                } 
+                // send product details to account service
+                sendUpdateUserData(formData, formHeaders, res);
+            } catch(error) {
+                console.error(error);
+                throw new Error(error); 
+            } 
+        });
     } catch (err) {
+        console.error(err);
+        sendJSONError(res, 500, true, "error occured while updating user", err); 
+    }
 
-        console.error(err) 
+    async function sendUpdateUserData(data, headers, res) {
+        try {
+            const updateUserResponse = await updateUser(data, headers);
 
+            if (updateUserResponse.error) {
+                throw new Error(updateUserResponse.error);
+            }
+            
+            if (io) {
+                io.sockets.emit('userDataChange');
+            }
+            return res.status(200).json(updateUserResponse);
+        } catch (err) {
+            console.error(err);
+            throw new Error(err);  
+        }
     } 
-
 }
 
 UserController.prototype.getUserNotifications = async function(req, res) {
+    const user = {
+        id: req.params.id,
+        userEmail: req.params.userEmail
+    }
 
     try {
-
-        const user = {
-            id: req.params.id,
-            userEmail: req.params.userEmail
-        }
-
         const userNotifications  = await getUserNotifications(user);
 
         res.status(200).json(userNotifications);  
-
     } catch (err) {
-
-        console.error(err) 
-
+        console.error(err);
+        sendJSONError(res, 500, true, "error occured while getting notifications", err);   
     }
-
 }
 
 UserController.prototype.getUserStars = async function(req, res) {
-
     try {
-
-        const userStarsResponse  = await getUserStars(req.params.userId);
+        const userStarsResponse = await getUserStars(req.params.userId);
 
         if (userStarsResponse.error) {
-
-            return  sendJSONError(res, 500, true, "error occured while getting user stars")
-
+            throw new Error(userStarsResponse.error);
         }
-
         res.status(200).json(userStarsResponse);
-
     } catch (err) {
-
-        sendJSONError(res, 500, true, "error occured while getting user stars")
-
+        console.error(err);
+        sendJSONError(res, 500, true, "error occured while getting user stars", err);
     } 
-
 }
 
 UserController.prototype.getDeliveryRegions = async function(req, res) {
-
     try {
-
-        const delieveryRegionsResponse  = await getDeliveryRegions(req.params.userId);
+        const delieveryRegionsResponse = await getDeliveryRegions(req.params.userId);
 
         if (delieveryRegionsResponse.error) {
-
-            return  sendJSONError(res, 500, true, "error occured while getting delivery regions")
-
+            throw new Error(delieveryRegionsResponse.error);
         }
-
         res.status(200).json(delieveryRegionsResponse);
-
     } catch (err) {
-
-        sendJSONError(res, 500, true, "error occured while getting delievery regions")
-
+        console.error(err);
+        sendJSONError(res, 500, true, "error occured while getting delievery regions", err);
     } 
-
 }
 
 UserController.prototype.getPreviousSearches = async function(req, res) {
-
     try {
-
-        const prevSearchResponse  = await getPreviousSearches(req.params.userId);
+        const prevSearchResponse = await getPreviousSearches(req.params.userId);
 
         if (prevSearchResponse.error) {
-
-            return  sendJSONError(res, 500, true, "error occured while getting delivery regions")
-
+            throw new Error(prevSearchResponse.error);
         }
-
         res.status(200).json(prevSearchResponse);
-
     } catch (err) {
-
-        sendJSONError(res, 500, true, "error occured while getting delievery regions")
-
+        sendJSONError(res, 500, true, "error occured while getting previous searches", err);
     } 
-
-}
-
-
-
-function sendJSONError(res, status, error, message) {
-
-    const response = {
-        status,
-        error, 
-        message, 
-    }
-
-    res.json(response) 
-
-    return res.status(500)
 }
 
 module.exports = UserController;
