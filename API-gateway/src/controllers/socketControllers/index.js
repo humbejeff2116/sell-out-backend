be19ae4bfaa7528f763a60da5e0d0a302c11b430
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-03 23:27:49 +0000

    rename ProductCommentController to ProductReviewController

--- API-gateway/src/controllers/socketControllers/index.js
+++ API-gateway/src/controllers/socketControllers/index.js
@@ -1,23 +1,18 @@
 
-
-
-
-
-
-
 const UserController = require('./userController');
 const PostFeedsController = require('./postFeedsController');
 const ProductController = require('./productsController');
 const ProductOrderController = require('./orderController');
-const ProductCommentController = require('./productCommentController');
+const ProductReviewController = require('./productCommentController');
 const UserDataChangeController = require('./userDataChangeController');
 const FeesController = require('./feesController');
+
 module.exports = {
     UserController,
     PostFeedsController,
     ProductController,
     ProductOrderController,
-    ProductCommentController,
+    ProductReviewController,
     UserDataChangeController,
     FeesController
 }
