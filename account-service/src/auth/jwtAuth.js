eb7d7bb60f78bcb074f3b3bd95d2996b715a0384
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 07:57:56 +0000

    refactor jwt Authentication functionality and remove excessive white spaces from module

--- account-service/src/auth/jwtAuth.js
+++ account-service/src/auth/jwtAuth.js
@@ -1,35 +1,31 @@
 
-
-
-
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 module.exports = (req, res, next) => {
-    
     const JWT_SECRET = config.secret.jwtSecret;
-    let token = req.body['x-access-token'] || req.query['x-access-token'] || req.headers['x-access-token'];
+    const token = req.body['x-access-token'] || req.query['x-access-token'] || req.headers['x-access-token'];
+
+    function verifyToken() {
+        jwt.verify(token, JWT_SECRET, function (err, decoded) {
+            if (err) {
+               res.send({status: 403, success: false,  message: 'Failed to authenticate token.'});
+               return res.status(403);
+            } 
+
+            req.decoded = decoded;
+            next();
+        });
+    }
+
+    function tokenNotPriveded() {
+        res.send({status: 401, success: false, message: 'No token provided.'});
+        return res.status(401);
+    }
+
     if (token) {
         verifyToken();
-
-        function verifyToken() {
-            jwt.verify(token, JWT_SECRET, function(err, decoded) {
-                if (err) {
-                   res.send({status:403, success: false,  message: 'Failed to authenticate token.' });
-                   return res.status(403);
-                } 
-                    req.decoded = decoded;
-                   next();
-            });
-        }
-
     } else {
         tokenNotPriveded();
-
-        function tokenNotPriveded() {
-            res.send({status:401, success: false, message: 'No token provided.'});
-            return res.status(401);
-        }
-       
     }
-};
+}
