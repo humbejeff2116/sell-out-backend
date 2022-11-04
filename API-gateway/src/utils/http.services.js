de3b4544ed79c2406f5eefc53fe77d2b10f1b33d
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 07:46:34 +0000

    implement getUserProducts, getSimilarProducts HTTP service helper functions

--- API-gateway/src/utils/http.services.js
+++ API-gateway/src/utils/http.services.js
@@ -9,99 +9,68 @@ const dataMergerServerURL = httpBase(DATA_MERGER_SERVER_URI);
 
 
 module.exports = {
-
     signupUser: async function (signupDetails) {
-
         const singupResponse = await loginServerURL.post(`/signup`, signupDetails );
-       
         return singupResponse.data;
-
     },
-
     loginUser: async function (loginDetails) {
-
         const loginResponse = await loginServerURL.post(`/login`, loginDetails);
-
         return loginResponse.data;
-
     },
-
     getProducts: async function () {
-
         const productsResponse = await productServerURL.get(`/products`);
-
-        return productsResponse.data;
-        
+        return productsResponse.data;  
+    },
+    getUserProducts: async function ({ userId, productCategory }) {
+        const productsResponse = await productServerURL.get(`/products/${userId}`);
+        return productsResponse.data;  
+    },
+    getSimilarProducts: async function ({
+        userId, 
+        userEmail, 
+        productCategory
+    }) {
+        const getSimilarProducts = await productServerURL.get(`/similar-products/${userId}/${userEmail}/${productCategory}`);
+        return getSimilarProducts.data;
     },
     createProduct: async function (data, headers) {
-
         const createdProductResponse = await productServerURL.post(`/product`, data, { headers: {...headers}});
-
         return createdProductResponse.data;
-
     },
     authenticateUser: async function (user) {
-
         const response = await loginServerURL.post(`/authenticate-user/`, user);
-
         return response.data;
-
     },
     getUserNotifications: async function (user) {
-
         const response = await loginServerURL.get(`/notifications/${user.id}/${user.userEmail}`);
-
         return response.data;
-
     },
     searchProducts : async function (query) {
-
         const searchProductsResponse = await productServerURL.get(`/search-products?q=${query}`);
- 
         return searchProductsResponse.data;
-
     },
-    getProductComments : async function(productId) {
-
+    getProductReviews: async function (productId) {
         const productCommentsResponse = await productServerURL.get(`/reviews/${productId}`);
-
         return productCommentsResponse.data;
-
     },
-    getUserStars : async function(userId) {
-
-        const userStarsResponse = await loginServerURL.get(`/stars/${userId}`);
-        
+    getUserStars: async function (userId) {
+        const userStarsResponse = await loginServerURL.get(`/stars/${userId}`);    
         return userStarsResponse.data
-
     },
-     getProductLikes : async function(productId) {
-
-        const productLikesResponse = await productServerURL.get(`/product-likes/${productId}`);
-        
+    getProductLikes: async function (productId) {
+        const productLikesResponse = await productServerURL.get(`/product-likes/${productId}`); 
         return productLikesResponse.data
-
     },
-    updateUser: async function(data, headers) {
-
+    updateUser: async function (data, headers) {
         const updateUserResponse = await loginServerURL.post(`/update-user`, data, { headers: {...headers}});
-
         return updateUserResponse.data;
-
     },
-     getDeliveryRegions : async function(userId) {
-
-        const userDeliveryRegionResponse = await loginServerURL.get(`/delivery-regions/${userId}`);
-        
+     getDeliveryRegions: async function (userId) {
+        const userDeliveryRegionResponse = await loginServerURL.get(`/delivery-regions/${userId}`);   
         return userDeliveryRegionResponse.data
-
     },
-    getPreviousSearches : async function(userId) {
-
-        const userPreviousSearchesResponse = await loginServerURL.get(`/user-prev-search/${userId}`);
-        
+    getPreviousSearches: async function (userId) {
+        const userPreviousSearchesResponse = await loginServerURL.get(`/user-prev-search/${userId}`);   
         return userPreviousSearchesResponse.data
-
     }
-
 }
