99d01567f14ce72885242e726092837486253636
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 00:22:34 +0000

    implement getUser, getReviewUser user socket events functionality (api-gateway)

--- API-gateway/src/libs/socketEventsHandlers/userSocketEventsHandler.js
+++ API-gateway/src/libs/socketEventsHandlers/userSocketEventsHandler.js
@@ -1,112 +1,87 @@
 
 function userSocketEventsHandler(io, socket, socketOptions, UserController) {
-    
     const userController = new UserController();
-
     userController.mountSocket(socketOptions);
 
-    socket.on('signUp', function(data) {
-
+    socket.on('signUp', function (data) {
         const signupData = {
             user: data,
             socketId: socket.id
         }
-
         return userController.signupUser(io, signupData);
-
     });
 
-
-    socket.on('login', function(data) {
-
+    socket.on('login', function (data) {
         const loginData = {
             user: data,
             socketId: socket.id
         }
-
         userController.loginUser(io, loginData);
-
     });
 
-    socket.on('getUserById', function(data) {
+    socket.on('getUser', function (data) {
+        const userData = {
+            user: data,
+            socketId: socket.id
+        }
+        userController.getUser(io, userData);
+    });
 
+    socket.on('getUserById', function (data) {
         const userData = {
             userId: data,
             socketId: socket.id
         }
-
         userController.getUserById(io, userData);
-
     });
 
     // star seller
-    socket.on('starSeller', function(data) {
-
+    socket.on('starSeller', function (data) {
         const socketId = socket.id;
-
         data.socketId = socketId;
-
         userController.starUser(io, data);
-
     });
 
     // get seller stars
-    socket.on('getInitialStarData', function(data) {
-
+    socket.on('getInitialStarData', function (data) {
         const socketId = socket.id;
-
         const productData = { socketId, product:data };
-
         userController.getUserStars(io, productData);
-
     });
 
     // get notifications
-    socket.on('getNotifications', function(data) {
-
+    socket.on('getNotifications', function (data) {
         const  user = data;
-
         const socketId = socket.id;
-
         const notificationData = {socketId, user};
-        
         userController.getNotifications(io, notificationData);
-
     });
 
     // seen notifications
-    socket.on('seenNotifications', function(data) {
-
+    socket.on('seenNotifications', function (data) {
         const  user = data;
-
         const socketId = socket.id;
-
         const notificationData = {socketId, user};
-
         userController.seenNotifications(io, notificationData);
-
     });
 
-    socket.on('getUserPreviousSearches', function(data) {
-
+    socket.on('getUserPreviousSearches', function (data) {
         const socketId = socket.id;
-
         data.socketId = socketId;
-
         userController.getUserPreviousSearches(io, socket, data);
-
     });
 
-    socket.on('removeUserSearch', function(data) {
-
+    socket.on('removeUserSearch', function (data) {
         const socketId = socket.id;
-
         data.socketId = socketId;
-
         userController.removeUserSearch(io, socket, data);
-
     });
 
+    socket.on('getReviewUser', function (data) {
+        const socketId = socket.id;
+        data.socketId = socketId;
+        userController.getReviewUser(io, socket, data);
+    });
 }
 
 module.exports.userSocketEventsHandler = userSocketEventsHandler;
