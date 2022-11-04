0bdc1a809bfcf287a7bb2a1e5eb51b6873f1358e
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 09:43:21 +0000

    implement orderDeliveryConfirmationSent product order socket event (account service)

--- account-service/src/libs/socketEventsHandlers/productOrderSocketEventHandler.js
+++ account-service/src/libs/socketEventsHandlers/productOrderSocketEventHandler.js
@@ -1,40 +1,31 @@
 
 function productOrderSocketEventsHandler(io, socket, socketOptions, ProductOrderController) {
-   
     const productOrderController = new ProductOrderController();
-
     productOrderController.mountSocket(socketOptions);
 
-    socket.on('confirmDelivery', function(data) {
-
+    socket.on('confirmDelivery', function (data) {
         return productOrderController.confirmDelivery(data);
-
     });
 
-    socket.on('orderCreated', function(data) {
-
+    socket.on('orderCreated', function (data) {
         return productOrderController.notifySellerAboutOrder(data, io);
-
     })
 
-    socket.on('productPaymentCreated', function(data) {
-
+    socket.on('productPaymentCreated', function (data) {
         return productOrderController.notifySellerABoutPendingPayment(data, io);
-
     })
 
-    socket.on('productDelivered', function(data) {
-
+    socket.on('productDelivered', function (data) {
         return productOrderController.addProductDeliveredNotification(data, io);
-
     })
     
-    socket.on('sellerPaymentFundsReleased', function(data) {
-
+    socket.on('sellerPaymentFundsReleased', function (data) {
         return productOrderController.addPaymentFundReleasedNotification(data, io);
-
     })
-  
+
+    socket.on('orderDeliveryConfirmationSent', function (data) {
+        return productOrderController.addConfirmOrderDeliveryRequestNotification(data, io);
+    })
 }
 
 module.exports.productOrderSocketEventsHandler = productOrderSocketEventsHandler;
