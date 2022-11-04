a0f3c799845895ad9029e02f5b7e3d85c32f5d60
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 00:08:03 +0000

    move reviewProduct, replyReview socket events into productSocketEventsHandler module and remove excessive white spaces

--- API-gateway/src/libs/socketEventsHandlers/productCommentSocketEventsHandler.js
+++ API-gateway/src/libs/socketEventsHandlers/productCommentSocketEventsHandler.js
@@ -1,81 +1,33 @@
-
-
-function productCommentSocketEventsHandler(io, socket, socketOptions, ProductCommentController) {
-   
-    const productCommentController = new ProductCommentController();
-
-    productCommentController.mountSocket(socketOptions);
-
-    // comment/review product
-    socket.on('reviewProduct', function(data) {
-      
-        const { product, reviewMessage, user } = data;
-
-        const socketId = socket.id;
-
-        const reviewData = {
-            user: user,
-            product: product,
-            reviewMessage: reviewMessage,
-            socketId :socketId
-        }
-
-        productCommentController.reviewProduct(io, socket, reviewData); 
-
-    });
-
-    // reply comment/review product
-    socket.on('replyReviewProduct', function(data) {
-      
-        const { commentId, replyMessage, user } = data;
-
-        const socketId = socket.id;
-
-        const replyReviewData = {
-            user: user,
-            commentId: commentId,
-            replyMessage:replyMessage,
-            socketId :socketId
-        }
-
-        productCommentController.replyReviewProduct(io, socket, replyReviewData); 
-
-    })
+// TODO... move remaining functionality into productSocketEventHandlker
+function productReviewSocketEventsHandler(io, socket, socketOptions, ProductReviewController) {
+    const productReviewController = new ProductReviewController();
+    productReviewController.mountSocket(socketOptions);
 
     // like comment
-    socket.on('likeReview', function(data) {
-      
+    socket.on('likeReview', function (data) {
         const { commentId, user } = data;
-
         const socketId = socket.id;
-        
         const likeCommentData = {
             user: user,
             commentId: commentId,
             socketId :socketId
         }
 
-        productCommentController.likeReview(io, socket, likeCommentData);  
-
+        productReviewController.likeReview(io, socket, likeCommentData);  
     });
 
     // unlike comment
-     socket.on('dislikeReview', function(data) {
-      
+     socket.on('dislikeReview', function (data) {
         const { commentId, user } = data;
-
         const socketId = socket.id;
-
         const unLikeCommentData = {
             user: user,
             commentId: commentId,
             socketId :socketId
         }
 
-        productCommentController.dislikeReview(io, socket, unLikeCommentData); 
-
+        productReviewController.dislikeReview(io, socket, unLikeCommentData); 
     });
-
 }
 
-module.exports.productCommentSocketEventsHandler = productCommentSocketEventsHandler;
+module.exports.productReviewSocketEventsHandler = productReviewSocketEventsHandler;
