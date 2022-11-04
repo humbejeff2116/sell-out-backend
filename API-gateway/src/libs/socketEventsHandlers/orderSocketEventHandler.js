1fc5f8806102d50bd89f5696eb84cc8db7f1b17a
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 00:00:06 +0000

    implement getUserProductOrderDeliveries order socket event (api-gtaeway)

--- API-gateway/src/libs/socketEventsHandlers/orderSocketEventHandler.js
+++ API-gateway/src/libs/socketEventsHandlers/orderSocketEventHandler.js
@@ -1,40 +1,32 @@
 function orderSocketEventsHandler(io, socket, socketOptions, OrderController) {
-
     // create order
     const productOrderController = new OrderController();
-
     productOrderController.mountSocket(socketOptions);
-
+    
     socket.on('createOrder', function(data) {
-
         data.socketId = socket.id; 
-
         productOrderController.createOrder(io, socket, data);
-
     });
 
     // get product orders
-    socket.on('getUserProductOrders', function(data) {
-
+    socket.on('getUserProductPlacedOrders', function(data) {
         data.socketId = socket.id; 
-
         console.log("getting product orders ---order socket events handler---");
+        productOrderController.getUserProductPlacedOrders(io, socket, data);
+    });
 
-        productOrderController.getUserProductOrders(io, socket, data);
-
+    socket.on('getUserProductOrderDeliveries', function(data) {
+        data.socketId = socket.id; 
+        console.log("getting product orders deliveries ---order socket events handler---");
+        productOrderController.getUserProductOrderDeliveries(io, socket, data);
     });
 
     // confirm delivery
     socket.on('confirmDelivery', function(data) {
-
         data.socketId = socket.id; 
-
         console.log("confirming delivery ---order socket events handler---");
-
         productOrderController.confirmDelivery(io, socket, data);
-
     });
-    
 }
 
 module.exports.orderSocketEventsHandler = orderSocketEventsHandler;
