23a1a2df5374ba911d94850e1a1217e234bf50e0
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-04 09:44:51 +0000

    remove excessive white spaces from productSocketEventsHandler (account service)

--- account-service/src/libs/socketEventsHandlers/productSocketEventHandler.js
+++ account-service/src/libs/socketEventsHandlers/productSocketEventHandler.js
@@ -1,16 +1,11 @@
 
 function productSocketEventsHandler(io, socket, socketOptions, ProductController) {
-
     const productController = new ProductController();
-
     productController.mountSocket(socketOptions);
 
-    socket.on('likeProductSuccess', function(data) {
-
+    socket.on('likeProductSuccess', function (data) {
         productController.addOrRemoveLikeProductNotification(data, io);
-
     });
-
 }
 
 module.exports.productSocketEventsHandler = productSocketEventsHandler;
