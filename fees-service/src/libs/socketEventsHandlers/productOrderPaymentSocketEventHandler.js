b735adafdcf931da404271f5fa973f8650cb9e5c
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-06 10:15:57 +0000

    rename getUserPayments socket event to getUserProductOrderPayments (fees service)

--- fees-service/src/libs/socketEventsHandlers/productOrderPaymentSocketEventHandler.js
+++ fees-service/src/libs/socketEventsHandlers/productOrderPaymentSocketEventHandler.js
@@ -1,28 +1,19 @@
 
 function productOrderPaymentSocketEventsHandler(io, socket, socketOptions, ProductOrderPaymentController) {
-
     const productOrderPaymentController = new ProductOrderPaymentController();
-
     productOrderPaymentController.mountSocket(socketOptions);
  
     socket.on('createPayment', function(data, callback) {
-
         productOrderPaymentController.createProductOrderPayment(io, socket, data, callback);
-
     });
 
     socket.on('productDelivered', function(data, callback) {
-
         productOrderPaymentController.paySellerAfterDelivery(io, socket, data, callback);
-
     });
 
-    socket.on('getUserPayments', function(data) {
-
-        productOrderPaymentController.getUserPayments(io, socket, data);
-
+    socket.on('getUserProductOrderPayments', function(data, callback) {
+        productOrderPaymentController.getUserProductOrderPayments(io, socket, data, callback);
     })
-
 }
 
 module.exports.productOrderPaymentSocketEventsHandler = productOrderPaymentSocketEventsHandler;
