fc0b42cdd21bc85fcb06bfe4f0b505cd652d0cc5
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-06 16:39:55 +0000

    remove un used socket events from event handler (product service)

--- product-service/src/libs/socketEventsHandlers/productCommentSocketEventsHandler.js
+++ product-service/src/libs/socketEventsHandlers/productCommentSocketEventsHandler.js
@@ -1,34 +1,15 @@
 
 function productCommentSocketEventsHandler(io, socket, socketOptions, ProductCommentController) {
-
     const productCommentController = new ProductCommentController();
-
     productCommentController.mountSocket(socketOptions);
 
-    socket.on('reviewProduct', function(data, callback = f =>f) {
-
-        productCommentController.reviewProduct(data, callback);
-
-    });
-
-    socket.on('replyReviewProduct', function(data, callback = f =>f) {
-
-        productCommentController.replyProductReview(data, callback);
-
-    });
-
-    socket.on('likeReview', function(data, callback = f =>f) {
-
+    socket.on('likeReview', function(data, callback = f => f) {
         productCommentController.likeReview(data, callback);
-
     });
 
-    socket.on('dislikeReview', function(data, callback = f =>f) {
-
+    socket.on('dislikeReview', function(data, callback = f => f) {
         productCommentController.dislikeReview(data, callback);
-
     });
-
 }
 
 module.exports.productCommentSocketEventsHandler = productCommentSocketEventsHandler;
