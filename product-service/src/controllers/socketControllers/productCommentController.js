d6eb5a4ab7576dc23458f3b5cc591e00dc32a3d0
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-06 16:20:55 +0000

    remove un used functionalities from product Comment Controller (product service)

--- product-service/src/controllers/socketControllers/productCommentController.js
+++ product-service/src/controllers/socketControllers/productCommentController.js
@@ -1,6 +1,8 @@
 
-const Product = require('../../models/productModel');
-const Comment = require('../../models/commentModel');
+// NOTE: this controller module/class has been retired
+// HINT: moved controller functionalities to product controller 
+// TODO... remove/delete module
+const Review = require('../../models/commentModel');
 
 /**
  * @class 
@@ -8,184 +10,56 @@ const Comment = require('../../models/commentModel');
  * @module ProductCommentController
  */
  function  ProductCommentController() {
-
     this.userClient = null;
-
     this.serverSocket = null;
-
 }
 
 /**
  * @method mountSocket 
  ** Used to initialize the class instance variables
- * @param {object} userClient - the socket.IO client of the product and service controller class
- * @param {object} serverSocket - the socket.IO server socket of the product-service server
+ * @param {object} userClient - the socket.IO client of the product controller class
+ * @param {object} serverSocket - the socket.IO server socket of this product-service server
  * 
  */
- ProductCommentController.prototype.mountSocket = function({ userClient, serverSocket}) {
-
-    this.userClient = userClient ? userClient : null;
-
-    this.serverSocket = serverSocket ? serverSocket : null;
-
+ ProductCommentController.prototype.mountSocket = function ({ userClient, serverSocket }) {
+    this.userClient = userClient || null;
+    this.serverSocket = serverSocket || null;
     return this;
-
 }
 
 /**
  * @method getSocketDetails  
  ** Used to get the service node socket datesils
  */
- ProductCommentController.prototype.getSocketDetails = function() {
-
+ ProductCommentController.prototype.getSocketDetails = function () {
     return ({
        userClient: this.userClient,
         serverSocket: this.serverSocket,
     });
-
 }
 
-/**
- * @method reviewProductOrService
- ** used to add review to a product or service
- ** initiates a server connection with login service node
- ** collects user data from login service
- ** emits a product or service not found error if ecountered
- ** add review to product or service
- ** sends back response to login service 
- * @param {object} data - the product data collected from login node 
- */
- ProductCommentController.prototype.reviewProduct =  async function({ product, socketId, user,  reviewMessage }, callback = f =>f) {
 
+ProductCommentController.prototype.likeReview = async function (data= {}, callback = f => f) {
+    const {socketId, commentId, user} = data;
     const self = this;
 
     try {
-        
-        const sellerProduct = await Product.getProductById(product.productId);
+        const review = await Review.getCommentById(commentId);
 
-        if (!sellerProduct) {
-
-            const response = {
-                status: 401,
-                error: true, 
-                socketId: socketId,
-                message: 'product not found',    
-            }
-
-            return callback(response);
-
-        }
-
-        const newComment = new Comment();
-
-        newComment.setProductCommentDetails({ product, socketId, user,  reviewMessage });
-
-        const savedComment = await newComment.save();
-
-        const response = {
-            status:201, 
-            error: false,
-            socketId: socketId,
-            comment: savedComment,
-            user: user, 
-            productOrService: sellerProduct,
-            product: sellerProduct,
-            message: 'product reviewed successfully', 
-        }
-
-        callback(response);
-
-        self.userClient.emit('reviewProductSuccess', response);
-
-    } catch(err) {
-
-        console.error(err)
-
-    }
-
-}
-
-ProductCommentController.prototype.replyProductReview =  async function({ commentId, user, socketId, replyMessage, replyTime }, callback = f =>f) {
-    
-    try {
-
-        const self = this;
-
-        const comment = await Comment.getCommentById(commentId);
-
-        if (!comment) {
-
-            const response = {
-                error:true,
-                status:401,
-                socketId: socketId,
-                message: `comment with id: ""${commentId}"" not found`,
-            }
-
-            callback(response);
-
-            console.error("no comment found");
-
-            return;
-
-        }
-       
-        const updatedComment = await Comment.addCommentReply({ commentId, user, socketId, replyMessage, replyTime });
-
-        if (updatedComment.status === 201 ) {
-
-            const response = {
-                error: false,
-                status: 201,
-                socketId: socketId,
-                user: user, 
-                comment: comment,
-                message: 'reply on comment successfull', 
-            }
-
-            callback(response);
-
-            self.userClient.emit('replyReviewProductSuccess', response);
-
-        }
-        
-    } catch(err) {
-
-        console.error(err)
-
-    }
-
-}
-
-ProductCommentController.prototype.likeReview = async function(data= {}, callback = f =>f) {
-
-    try {
-
-        const {socketId, commentId, user} = data;
-
-        const self = this;
-
-        const comment = await Comment.getCommentById(commentId);
-
-        if (!comment) {
-
+        if (!review) {
             const response = {
                 socketId: socketId,
                 error: true,
                 status: 401,
                 message: "comment not found"
             }
-
             return callback(response);
-
         }
         
-        const updateCommentLike = await Comment.addCommentLike(data);
-
-        const updateCommentDislike = await Comment.removeCommentDislike(data);
+        const updateCommentLike = await Review.addCommentLike(data);
+        const updateCommentDislike = await Review.removeCommentDislike(data);
 
         if ((updateCommentLike.status === 201) && (updateCommentDislike.status === 201)) {
-
             const response = {
                 socketId: socketId,
                 user: user,
@@ -194,50 +68,35 @@ ProductCommentController.prototype.likeReview = async function(data= {}, callbac
                 status: 201,
                 message: "review liked successfully"
             }
-
             callback(response);
-
             self.userClient.emit('likeReviewSuccess', response);
-
         }
-    
     } catch (err) {
-
         console.error(err)  
-
     } 
-
 }
 
 ProductCommentController.prototype.dislikeReview = async function(data= {}, callback = f =>f) {
 
     try {
-
         const {socketId, commentId, user} = data;
-
         const self = this;
-
-        const comment = await Comment.getCommentById(commentId);
+        const comment = await Review.getCommentById(commentId);
 
         if (!comment) {
-
             const response = {
                 socketId: socketId,
                 error: true,
                 status: 401,
                 message: "comment not found"
             }
-
             return callback(response);
-
         }
         
-        const addCommentDislike = await Comment.addCommentDislike(data);
-
-        const removeCommentDislike = await Comment.removeCommentLike(data);
+        const addCommentDislike = await Review.addCommentDislike(data);
+        const removeCommentDislike = await Review.removeCommentLike(data);
 
         if ((addCommentDislike.status === 201) &&(removeCommentDislike.status === 201)) {
-
             const response = {
                 socketId: socketId,
                 user: user,
@@ -246,19 +105,13 @@ ProductCommentController.prototype.dislikeReview = async function(data= {}, call
                 status: 401,
                 message: "comment disliked successfully"
             }
-    
             callback(response);
-    
             self.userClient.emit('dislikeReviewSuccess', response);
-
         }
 
     } catch (err) {
-
         console.error(err) 
-
     }
-
 }
 
 module.exports = ProductCommentController;
