a78f545adbfc16b23feda80fb6701b98f5a01bd7
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-06 11:04:18 +0000

    implement getUserProductPlacedOrders, getUserProductOrderDeliveries order socket events (order service)

--- order-service/src/libs/socketEventsHandlers/orderSocketEventHandler.js
+++ order-service/src/libs/socketEventsHandlers/orderSocketEventHandler.js
@@ -1,28 +1,22 @@
 
 function orderSocketEventsHandler(io, socket, socketOptions, ProductOrderController) {
-
     const productOrderController = new ProductOrderController();
-
     productOrderController.mountSocket(socketOptions);
 
-    socket.on('createOrder', function(data, callback= f =>f) {
-
-        productOrderController.createOrder(io, socket, data, callback= f =>f);
-
+    socket.on('createOrder', function (data, callback= f => f) {
+        productOrderController.createOrder(io, socket, data, callback);
     });
 
-    socket.on('getUserProductOrders', function(data, callback= f =>f) {
-
-        productOrderController.getUserProductOrders(io, socket, data, callback= f =>f);
-
+    socket.on('getUserProductPlacedOrders', function (data, callback= f => f) {
+        productOrderController.getUserProductPlacedOrders(io, socket, data, callback);
     });
 
-    socket.on('confirmDelivery', function(data, callback= f =>f) {
-
-        productOrderController.confirmDelivery(io, socket, data, callback= f =>f);
-
+    socket.on('getUserProductOrderDeliveries', function (data, callback= f => f) {
+        productOrderController.getUserProductOrderDeliveries(io, socket, data, callback);
     });
 
+    socket.on('confirmDelivery', function (data, callback= f => f) {
+        productOrderController.confirmDelivery(io, socket, data, callback);
+    });
 }
-
 module.exports.orderSocketEventsHandler = orderSocketEventsHandler;
