2f44c07f0a61e5352cad8488ed942b850b3c70e3
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-06 10:34:26 +0000

    implement getBuyerPaymentsById, getSellerPaymentsById static methods of Payment model and remove excessive white spaces from module

--- fees-service/src/models/paymentModel.js
+++ fees-service/src/models/paymentModel.js
@@ -1,8 +1,6 @@
 
 const mongoose = require('mongoose');
-
 const PaymentSchema =  mongoose.Schema({
-
     placedOrderId: { type: String, required: true },
     orderId: { type: String, required: true },
     orderTime: { type: String, required: true },
@@ -17,108 +15,127 @@ const PaymentSchema =  mongoose.Schema({
     sellerRecievedPayment: { type: Boolean, default: false },
     productsSellerSold: [{}],
     createdAt: { type: Date, default: Date.now }
-
 });
 
-PaymentSchema.methods.setPaymentDetails = function(data, placedOrderId) {
+PaymentSchema.methods.setPaymentDetails = function (data, placedOrderId) {
+    const {
+        orderId, 
+        orderTime, 
+        sellerId, 
+        sellerName, 
+        sellerEmail, 
+        buyerId, 
+        buyerName, 
+        buyerEmail, 
+        totalAmount,
+        productsSellerSold 
+    } = data;
 
     this.placedOrderId = placedOrderId;
-    this.orderId = data.orderId;
-    this.orderTime = data.orderTime;
-    this.sellerId = data.sellerId;
-    this.sellerName = data.sellerName;
-    this.sellerEmail = data.sellerEmail;
-    this.buyerId = data.buyerId;
-    this.buyerName = data.buyerName;
-    this.buyerEmail = data.buyerEmail;
-    this.paymentAmount = data.totalAmount;
-    this.productsSellerSold = data.productsSellerSold; 
-
+    this.orderId = orderId;
+    this.orderTime = orderTime;
+    this.sellerId = sellerId;
+    this.sellerName = sellerName;
+    this.sellerEmail = sellerEmail;
+    this.buyerId = buyerId;
+    this.buyerName = buyerName;
+    this.buyerEmail = buyerEmail;
+    this.paymentAmount = totalAmount;
+    this.productsSellerSold = productsSellerSold; 
 }
 
-PaymentSchema.statics.updatePaymentStatus = async function({ 
-
+PaymentSchema.statics.updatePaymentStatus = async function ({ 
     placedOrderId, 
     sellerEmail, 
     sellerId,  
     paymentReleaseStatus = "released", 
     sellerRecievedPayment = true 
-
 }) {
-
     const updatePaymentReleaseStatus = await this.updateOne(
-
         { $and: [{sellerEmail: sellerEmail}, { placedOrderId: placedOrderId }] },
         { "$set": {"paymentReleaseStatus":  paymentReleaseStatus, "sellerRecievedPayment": sellerRecievedPayment } }
-
     )
-
     return updatePaymentReleaseStatus;
-
 }
 
-PaymentSchema.statics.getSellerPayment = function({ placedOrderId, sellerEmail, sellerId }) {
-
-    let payment = this.findOne({
-
-        $and:[
-            {sellerEmail: sellerEmail}, { placedOrderId: placedOrderId }
-        ]
-
+PaymentSchema.statics.getSellerPayment = async function ({ placedOrderId, sellerEmail, sellerId }) {
+    const payment = await this.findOne({
+        $and: [{sellerEmail: sellerEmail}, {placedOrderId: placedOrderId}]
     });
 
     return payment;
-
 }
 
-PaymentSchema.statics.getSellerPayments = function(data) {
-
-    const {userEmail} = data
-
-    let payments = this.find({userEmail});
+PaymentSchema.statics.getSellerPayments = async function (data) {
+    const { userEmail } = data
+    const payments = await this.find({userEmail});
 
     return payments;
-
 }
 
-PaymentSchema.statics.getBuyerPayments = function(data) {
-
-    const {userEmail} = data
-
-    let payments = this.find({userEmail});
+PaymentSchema.statics.getBuyerPayments = async function (data) {
+    const { userEmail } = data
+    const payments = await this.find({userEmail});
 
     return payments;
-
 }
 
-PaymentSchema.statics.getSellerPaymentsByEmailOrId = async function({sellerEmail, sellerId}) {
-
+PaymentSchema.statics.getSellerPaymentsByEmailOrId = async function ({ sellerEmail, sellerId }) {
     const payments = await this.find({
-
-        $or:[
-            {sellerEmail: sellerEmail}, { sellerId: sellerId }
-        ]
-
+        $or: [{sellerEmail: sellerEmail}, {sellerId: sellerId}]
     });
 
     return payments;
-
 }
 
-PaymentSchema.statics.getBuyerPaymentsByEmailOrId = async function({buyerEmail, buyerId}) {
-
+PaymentSchema.statics.getBuyerPaymentsByEmailOrId = async function ({ buyerEmail, buyerId }) {
     const payments = await this.find({
-
-        $or:[
-            {buyerEmail: buyerEmail}, { buyerId: buyerId }
-        ]
-
+        $or: [{buyerEmail: buyerEmail}, {buyerId: buyerId}]
     });
 
     return payments;
+}
 
+PaymentSchema.statics.getSellerPaymentsById = async function ({ sellerId }) {
+    try {
+        const [pendingPayments, recievedPayments] = await Promise.all([
+            this.find({
+                $and: [{sellerId: sellerId}, {sellerRecievedPayment: false}]
+            }),
+            this.find({ 
+                $and: [{ sellerId: sellerId }, {sellerRecievedPayment: true}]
+            })
+        ])
+    
+        return ({
+            pending: pendingPayments,
+            recieved: recievedPayments 
+        })   
+    } catch (err) {
+        throw new Error(err);
+    }
+}
+
+PaymentSchema.statics.getBuyerPaymentsById = async function ({ buyerId }) {
+    try {
+        const [unReleasedPayments, releasedPayments] = await Promise.all([
+            this.find({
+                $and: [{buyerId : buyerId}, {sellerRecievedPayment: false}]
+            }),
+            this.find({
+                $and: [{buyerId : buyerId}, {sellerRecievedPayment: true}]
+            })
+        ])
+    
+        return ({
+            unReleased: unReleasedPayments,
+            released: releasedPayments 
+        })
+ 
+    } catch (err) {
+        throw new Error(err);
+    }
 }
 
 const Payment = mongoose.model('payments', PaymentSchema);
-
 module.exports = Payment;
