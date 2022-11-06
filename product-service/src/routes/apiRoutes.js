677b6511576381cfabf450ee309cdcdaef46870f
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-06 16:49:59 +0000

    remove reviews and search-products routes (product service)

--- product-service/src/routes/apiRoutes.js
+++ product-service/src/routes/apiRoutes.js
@@ -7,8 +7,7 @@ const router = express.Router();
 
 router.post('/product', multerUploads, ProductController.createProduct);
 router.get('/products', ProductController.getProducts);
-router.get('/reviews/:productId', ProductController.getProductComments);
-router.get('/search-products', ProductController.searchProducts);
+router.get('/similar-products/:userId/:userEmail/:productCategory', ProductController.getSimilarProducts);
 router.get('/product-likes/:productId', ProductController.getProductLikes);
 
 module.exports = router;
